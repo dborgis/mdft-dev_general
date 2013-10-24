1d0367a328aa3fae8e380d41f76648d4b0e7a441
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-10-24 22:02:57 +0200

    pretify

--- src/energy_and_gradient.f90
+++ src/energy_and_gradient.f90
@@ -2,6 +2,7 @@
 ! first is computed the radial_part, then ... blabla.
 ! this subroutine is the one called by the minimization stuff
 ! for computing the total energy and associated gradient
+
 subroutine energy_and_gradient
 
     use precision_kinds , only : i2b , dp
@@ -18,47 +19,47 @@ subroutine energy_and_gradient
     logical :: hydrophobicity_tag
 
 
-! FF is the TOTAL ENERGY of the system, it is thus the functional of the density that is minimized by solver
-! dF is the gradient of FF with respect to all coordinates. Remember it is of the kind dF ( number of variables over density (ie angles etc))
+    ! FF is the TOTAL ENERGY of the system, it is thus the functional of the density that is minimized by solver
+    ! dF is the gradient of FF with respect to all coordinates. Remember it is of the kind dF ( number of variables over density (ie angles etc))
+    call init_functional_and_gradient_to_zero( FF, dF )
 
-call init_functional_and_gradient_to_zero( FF, dF )
+    if (.not. allocated (rho_c_k_myway)) allocate (rho_c_k_myway(nfft1/2+1, nfft2, nfft3))
+    rho_c_k_myway=(0.0_dp, 0.0_dp)
 
-if (.not. allocated (rho_c_k_myway)) allocate (rho_c_k_myway(nfft1/2+1, nfft2, nfft3))
-rho_c_k_myway=(0.0_dp, 0.0_dp)
+    call energy_external ! compute ideal part of the total energy
+    call energy_ideal
 
-call energy_external ! compute ideal part of the total energy
-call energy_ideal
+    ! compute radial part of the excess free energy
+    if (input_log('hard_sphere_fluid') ) call energy_hard_sphere_fmt ! => pure hard sphere contribution
 
-! compute radial part of the excess free energy
-if (input_log('hard_sphere_fluid') ) call energy_hard_sphere_fmt ! => pure hard sphere contribution
+    ! test if there is a LJ perturbation to hard spheres ! WCA model etc. to implement more intelligently
+    if (input_log('lennard_jones_perturbation_to_hard_spheres') ) call lennard_jones_perturbation_to_hard_spheres ! lennard jones perturbative contribution => Weeks-Chandler-Anderson
 
-! test if there is a LJ perturbation to hard spheres ! WCA model etc. to implement more intelligently
-if (input_log('lennard_jones_perturbation_to_hard_spheres') ) call lennard_jones_perturbation_to_hard_spheres ! lennard jones perturbative contribution => Weeks-Chandler-Anderson
-
-if (input_log('read_ck_or_chi')) then
-  if (input_log('hydrophobicity')) then
-    if (trim(adjustl(input_char('treatment_of_hydro')))=='C')  then
-      call cs_plus_hydro
-    else if (trim(adjustl(input_char('treatment_of_hydro')))=='VdW')  then
-      call energy_hydro
+    if (input_log('read_ck_or_chi')) then
+        if (input_log('hydrophobicity')) then
+            if (trim(adjustl(input_char('treatment_of_hydro')))=='C')  then
+                call cs_plus_hydro
+            else if (trim(adjustl(input_char('treatment_of_hydro')))=='VdW')  then
+                call energy_hydro
+            end if
+        else
+            call cs_from_dcf
+        end if
     end if
-  else
-  call cs_from_dcf
-  end if
-end if
 
-! bridge calculation: F(FMT)-F(c2hs)+F(c2H2O)
-if (input_log('bridge_hard_sphere')) call energy_cs_hard_sphere ! better name should be given
+    ! bridge calculation: F(FMT)-F(c2hs)+F(c2H2O)
+    if (input_log('bridge_hard_sphere')) call energy_cs_hard_sphere ! better name should be given
 
-! Dipolar polarization. What user wants (use it or not) is checked in subroutine for clearer code.
-call energy_polarization
-call energy_polarization_myway
-! Threebody term that is needed to empiricaly force H-bonding in water. What user wants (use it or not) is checked in subroutine for clearer code
-call energy_threebody
-call energy_threebody_faster
-! inform user
-write(*,*) 'SOLVATION FREE ENERGY AT THIS STEP = ',FF
-write(*,*)'-----------------------'
+    ! Dipolar polarization. What user wants (use it or not) is checked in subroutine for clearer code.
+    call energy_polarization
+    call energy_polarization_myway
+
+    ! Threebody term that is needed to empiricaly force H-bonding in water. What user wants (use it or not) is checked in subroutine for clearer code
+    call energy_threebody
+    call energy_threebody_faster
+
+    write(*,*)'SOLVATION FREE ENERGY AT THIS STEP = ',FF
+    write(*,*)'-----------------------'
 
     contains
     
