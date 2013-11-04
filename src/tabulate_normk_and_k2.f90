d3a5fa55c6969f8422a25adf468ac556a2588366
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-05 00:51:26 +0100

    Refrac

--- src/tabulate_normk_and_k2.f90
+++ src/tabulate_normk_and_k2.f90
@@ -8,46 +8,42 @@ SUBROUTINE tabulate_normk_and_k2
     IMPLICIT NONE
 
     INTEGER(i2b) :: nfft1, nfft2, nfft3
-    INTEGER(i2b):: l , m , n , m1 , m2 , m3
-    REAL(dp) :: lx, ly, lz
+    INTEGER(i2b):: l, m1
     REAL(dp), PARAMETER :: twopi = ACOS(-1._dp)*2._dp
 
     nfft1 = spaceGrid%n_nodes(1)
     nfft2 = spaceGrid%n_nodes(2)
     nfft3 = spaceGrid%n_nodes(3)
-    lx = spaceGrid%length(1)
-    ly = spaceGrid%length(2)
-    lz = spaceGrid%length(3)
 
     ALLOCATE ( kx ( nfft1/2+1 ) )
     ALLOCATE ( ky ( nfft2 ) )
     ALLOCATE ( kz ( nfft3 ) )
 
-    DO CONCURRENT ( n=1:nfft3 )
-        IF ( n <= nfft3/2 ) THEN
-            m3 = n - 1
-        ELSE
-            m3 = n - 1 - nfft3
-        END IF
-        kz(n) = twopi/Lz*real(m3,dp)
-    END DO
-    
-    DO CONCURRENT ( m=1:nfft2 )
-        IF ( m <= nfft2/2 ) THEN
-            m2 = m - 1
-        ELSE
-            m2 = m - 1 - nfft2
-        END IF
-        ky(m) = twopi/Ly*real(m2,dp)
-    END DO
-    
     DO CONCURRENT ( l=1:nfft1/2+1 )
         IF ( l <= nfft1/2 ) THEN
             m1 = l - 1
         ELSE
             m1 = l - 1 - nfft1
         END IF
-        kx(l) = twopi/Lx*real(m1,dp)
+        kx(l) = twopi*REAL(m1,dp)/spaceGrid%length(1)
+    END DO
+
+    DO CONCURRENT ( l=1:nfft2 )
+        IF ( l <= nfft2/2 ) THEN
+            m1 = l - 1
+        ELSE
+            m1 = l - 1 - nfft2
+        END IF
+        ky(l) = twopi*REAL(m1,dp)/spaceGrid%length(2)
+    END DO
+    
+    DO CONCURRENT ( l=1:nfft3 )
+        IF ( l <= nfft3/2 ) THEN
+            m1 = l - 1
+        ELSE
+            m1 = l - 1 - nfft3
+        END IF
+        kz(l) = twopi*REAL(m1,dp)/spaceGrid%length(3)
     END DO
     
 END SUBROUTINE tabulate_normk_and_k2
