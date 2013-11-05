f4d536bb68b5e448b48c8e644e53b419c9a5fd0e
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-05 14:18:40 +0100

    refrac

--- src/electrostatic_potential_from_charge_density.f90
+++ src/electrostatic_potential_from_charge_density.f90
@@ -4,54 +4,57 @@
 ! => laplacien(V(r))=-rho_c(r)/eps0
 ! => V(k)=rho_c(k)/(esp0*k^2)
 ! FFT(V(k)) = V(r)
-subroutine electrostatic_potential_from_charge_density
-use precision_kinds , only : dp , i2b
-use system , only : nfft1 , nfft2 , nfft3 , rho_c
-! nfft = number of FFT grid nodes in each direction
-! rho_c (nfft1,nfft2,nfft3) = discrete charge density per unit volume
-use fft , only : in_forward , in_backward , out_forward , out_backward , plan_forward , plan_backward , norm_k , k2
-use constants , only : fourpi , twopi
-use external_potential , only : V_c
-! V_c = electrostatic potential from charge density and poisson equation
-implicit none
-complex(dp), dimension ( nfft1 / 2 + 1 , nfft2 , nfft3 ) :: rho_c_k
-complex(dp), dimension ( nfft1 / 2 + 1 , nfft2 , nfft3 ) :: V_c_k
-integer (i2b) :: i,j,k
+SUBROUTINE electrostatic_potential_from_charge_density
 
-! check if rho_c exists and is allocated
-if ( .not. allocated ( rho_c ) ) then
-  print *, 'rho_c is not allocated in electrostatic_potential_from_charge_density.f90'
-  stop
-end if
-if (maxval(abs(rho_c)) < tiny(1.0_dp)) then
-allocate ( V_c ( nfft1 , nfft2 , nfft3 ), source=0._dp )
-!~ v_c = 0.0_dp
-return
-end if
-! FFT of rho_c
-in_forward = rho_c
-call dfftw_execute ( plan_forward )
-rho_c_k = out_forward ! It is verified that at this point, FFT-1(rho_c_k)/ (nfft1*nfft2*nfft3) = rho_c
-! FFT(Laplacian(V(r))) = FFT( - 4Pi charge density(r) ) in elecUnits = (ik)^2 V(k) = -4pi rho(k)
-! V(k) = 4Pi rho(k) / k^2
+    USE precision_kinds , ONLY : dp , i2b
+    USE system , ONLY : nfft1 , nfft2 , nfft3 , rho_c
+    ! nfft = number of FFT grid nodes in each direction
+    ! rho_c (nfft1,nfft2,nfft3) = discrete charge density per unit volume
+    USE fft , ONLY : in_forward , in_backward , out_forward , out_backward , plan_forward , plan_backward , norm_k , k2
+    USE constants , ONLY : fourpi , twopi
+    USE external_potential , ONLY : V_c
+    ! V_c = electrostatic potential from charge density and poisson equation
+    
+    IMPLICIT NONE
+    COMPLEX(dp), DIMENSION ( nfft1 / 2 + 1 , nfft2 , nfft3 ) :: rho_c_k
+    COMPLEX(dp), DIMENSION ( nfft1 / 2 + 1 , nfft2 , nfft3 ) :: V_c_k
+    INTEGER (i2b) :: i,j,k
 
-DO CONCURRENT ( i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3 )
-    IF ( K2(i,j,k) /= 0._dp ) THEN
-        V_c_k(i,j,k) = rho_c_k(i,j,k) * fourpi/k2(i,j,k) ! in electrostatic units : V=-4pi rho
-    ELSE
-        V_c_k(i,j,k) = 0._dp
+    ! check if rho_c exists and is allocated
+    IF ( .NOT. ALLOCATED ( rho_c ) ) THEN
+        PRINT*, 'rho_c is not allocated in electrostatic_potential_from_charge_density.f90'
+        STOP
     END IF
-END DO
 
-! get real space potential V(r)
-if ( .not. allocated ( V_c ) ) allocate ( V_c ( nfft1 , nfft2 , nfft3 ) )
-in_backward = V_c_k
-call dfftw_execute ( plan_backward )
-V_c = out_backward / real ( nfft1 * nfft2 * nfft3 , dp )
-open(11,file='output/V_cmax.dat')
-do i=1, nfft1
-write(11,*), i , V_c(i, nfft2/2, nfft3/2 )
- 
-end do
-close(11)
-end subroutine electrostatic_potential_from_charge_density
+    IF ( MAXVAL(ABS(rho_c)) < TINY(1.0_dp)) THEN
+        ALLOCATE ( V_c ( nfft1 , nfft2 , nfft3 ), SOURCE=0._dp ) !~ v_c = 0.0_dp
+    END IF
+
+    ! FFT of rho_c
+    in_forward = rho_c
+    CALL dfftw_execute ( plan_forward )
+    rho_c_k = out_forward ! It is verified that at this point, FFT-1(rho_c_k)/ (nfft1*nfft2*nfft3) = rho_c
+    ! FFT(Laplacian(V(r))) = FFT( - 4Pi charge density(r) ) in elecUnits = (ik)^2 V(k) = -4pi rho(k)
+    ! V(k) = 4Pi rho(k) / k^2
+
+    DO CONCURRENT ( i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3 )
+        IF ( K2(i,j,k) /= 0._dp ) THEN
+            V_c_k(i,j,k) = rho_c_k(i,j,k) * fourpi/k2(i,j,k) ! in electrostatic units : V=-4pi rho
+        ELSE
+            V_c_k(i,j,k) = 0._dp
+        END IF
+    END DO
+
+    ! get real space potential V(r)
+    IF ( .NOT. ALLOCATED ( V_c ) ) ALLOCATE ( V_c ( nfft1 , nfft2 , nfft3 ) )
+    in_backward = V_c_k
+    CALL dfftw_execute ( plan_backward )
+    V_c = out_backward / REAL ( nfft1 * nfft2 * nfft3 , dp )
+    
+    OPEN(11,FILE='output/V_cmax.dat')
+        DO i=1, nfft1
+            WRITE(11,*), i , V_c(i, nfft2/2, nfft3/2 )
+        END DO
+    CLOSE(11)
+
+END SUBROUTINE electrostatic_potential_from_charge_density
