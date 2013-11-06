567490dbb03cbaf5681b1afb263cc2d888222cd7
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-06 09:07:06 +0100

    move unused module_threebody to trash

--- src/module_threebody.f90
+++
@@ -1,7 +0,0 @@
-module threebody
-use precision_kinds, only:dp,i2b
-implicit none
-real(dp), dimension(nfft1,nfft2,nfft3) :: Axx,Ayy,Azz,Axy,Axz,Ayz,Ax,Ay,Az,A0
-complex(dp), dimension(nfft1/2+1,nfft2,nfft3) :: Axx_k,Ayy_k,Azz_k,Axy_k,Axz_k,Ayz_k,Ax_k,Ay_k,Az_k,A0_k
-real(dp), dimension(nfft1,nfft2,nfft3) :: Gxx,Gyy,Gzz,Gxy,Gxz,Gyz,Gx,Gy,Gz,G0 
-end module
