2f8e2b7707e3fc41b52a0211f42a909cbc6b1a40
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-06 09:13:16 +0100

    refrac

--- src/close_simu.f90
+++ src/close_simu.f90
@@ -1,11 +1,11 @@
-!> This subroutine closes the program properly.
-!! It deallocates memory and destroys FFTW3 plans.
-subroutine close_simu
-use cg , only : deallocate_everything_cg
-use fft , only : deallocate_everything_fft
-use external_potential , only : deallocate_everything_external_potential
-implicit none
-call deallocate_everything_external_potential
-call deallocate_everything_fft
-call deallocate_everything_cg
-end subroutine close_simu
+! This subroutine closes the program properly.
+! It deallocates memory and destroys FFTW3 plans.
+SUBROUTINE close_simu
+    USE cg, ONLY : deallocate_everything_cg
+    USE fft, ONLY : deallocate_everything_fft
+    USE external_potential, ONLY : deallocate_everything_external_potential
+    IMPLICIT NONE
+    CALL deallocate_everything_external_potential
+    CALL deallocate_everything_fft
+    CALL deallocate_everything_cg
+END SUBROUTINE close_simu
