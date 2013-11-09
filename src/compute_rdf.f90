a4466b5f310e681a7a2367d6d144264a4fc54eec
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-09 23:57:38 +0100

    more use of derived types

--- src/compute_rdf.f90
+++ src/compute_rdf.f90
@@ -1,25 +1,24 @@
 ! This subroutine computes the radial distribution function around each solute site
 subroutine compute_rdf ( array , filename )
-use precision_kinds , only : dp , i2b
-use system , only : nfft1 , nfft2 , nfft3 , x_mol , y_mol , z_mol , id_mol , id_solv, nb_species, spaceGrid, soluteSite
-use input , only : input_line, input_dp, input_int
-IMPLICIT NONE
 
-REAL(dp), DIMENSION(spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3),nb_species), INTENT(IN) :: array
-CHARACTER( 50 ) , intent(in) :: filename
-INTEGER(i2b):: nb_id_solv ! Total number of solvent site kinds
-INTEGER(i2b):: nb_id_mol ! Total number of solute site kinds
-INTEGER(i2b):: nbins !> Total number of bins in which rdf is histogrammed
-REAL(dp):: RdfMaxRange !> Maximum distance to which calculate radial distribution function
-REAL(dp):: delta_r !> Width of each bin
-REAL(dp):: r !> Distance ^2 between each of the n solute sites and each grid point i,j,k
-REAL(dp), allocatable , dimension ( : , : ) :: rdf
-INTEGER(i2b), allocatable , dimension ( : , : ) :: recurrence_bin
-INTEGER(i2b):: i , j , k , n , bin
-REAL(dp):: xnm2 , ynm2 , znm2 ! dummy
-INTEGER(i2b):: species, nb_solute_sites ! dummy between 1 and nb_species
+    USE precision_kinds, ONLY: dp , i2b
+    USE system, ONLY: nfft1 , nfft2 , nfft3 , x_mol , y_mol , z_mol , id_mol , id_solv, nb_species, spaceGrid, soluteSite
+    USE input, ONLY: input_dp, input_int
+    
+    IMPLICIT NONE
 
-nb_solute_sites = SIZE(soluteSite)
+    REAL(dp), DIMENSION(spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3),nb_species), INTENT(IN) :: array
+    CHARACTER( 50 ) , intent(in) :: filename
+    INTEGER(i2b):: nb_id_solv ! Total number of solvent site kinds
+    INTEGER(i2b):: nb_id_mol ! Total number of solute site kinds
+    INTEGER(i2b):: nbins !> Total number of bins in which rdf is histogrammed
+    REAL(dp):: RdfMaxRange !> Maximum distance to which calculate radial distribution function
+    REAL(dp):: delta_r !> Width of each bin
+    REAL(dp):: r !> Distance ^2 between each of the n solute sites and each grid point i,j,k
+    REAL(dp), ALLOCATABLE, DIMENSION (:,:) :: rdf
+    INTEGER(i2b), ALLOCATABLE, DIMENSION (:,:) :: recurrence_bin
+    INTEGER(i2b):: i , j , k , n , bin, s
+    REAL(dp):: xnm2 , ynm2 , znm2 ! dummy
 
 !~ do i=1,4
 !~ PRINT*,FILENAME,size(array,i)
@@ -43,16 +42,17 @@ open ( 10 , file = filename , form = 'formatted' )
 100 format ( 2 ( xF10.5 ) )
 write ( 10 , * ) '# r  rdf'
 ! prepare histogram
-allocate ( recurrence_bin ( nb_solute_sites , 0 : nbins ) )
-allocate ( rdf ( nb_solute_sites , 0 : nbins ) )
+allocate ( recurrence_bin ( SIZE(soluteSite), 0:nbins ) )
+allocate ( rdf ( SIZE(soluteSite), 0:nbins ) )
+
 ! FOR EACH SPECIES
-do species = 1 , nb_species
+do s = 1 , nb_species
   recurrence_bin = 0
   rdf = 0.0_dp
-  write ( 10 , * ) '# species number ', species
+  write ( 10 , * ) '# species number ', s
   !> Transform array(position) in rdf(radialdistance)
   ! counts the total number of appearence of a value in each bin
-  do n = 1 , nb_solute_sites
+  do n = 1 , SIZE(soluteSite)
     do i = 1 , spaceGrid%n_nodes(1)
       xnm2 = ( REAL ( i - 1 , dp ) * spaceGrid%dl(1) - x_mol ( n ) ) ** 2
       do j = 1 , spaceGrid%n_nodes(2)
@@ -64,7 +64,7 @@ do species = 1 , nb_species
           bin = int ( r / delta_r + 0.5_dp ) !> In which bin of histogram are we.
           if ( bin > nbins ) cycle ! out of range
           recurrence_bin ( n , bin ) = recurrence_bin ( n , bin ) + 1
-          rdf ( n , bin ) = rdf ( n , bin ) + array ( i , j , k , species )
+          rdf ( n , bin ) = rdf ( n , bin ) + array ( i , j , k , s )
         end do
       end do
     end do
@@ -77,7 +77,7 @@ do species = 1 , nb_species
      rdf = 0.0_dp
   end where
   ! Write rdf in output folder
-  do n = 1 , nb_solute_sites
+  do n = 1 , SIZE(soluteSite)
      write ( 10 , * ) '#site ' , n
      do bin = 0 , nbins
            write ( 10 , 100 ) (REAL ( bin , dp )) * delta_r , rdf ( n , bin ) 
@@ -88,6 +88,7 @@ do species = 1 , nb_species
   write ( 10 , * )
 end do
 close(10)
+
 deallocate ( recurrence_bin )
 deallocate ( rdf )
 
