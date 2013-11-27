0e7dd7c6cda8fc05c95dc1300bf05b14a82f7c19
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-11-27 02:00:35 +0100

    refrac. still unoptimized because unused var stay. see list of issues

--- src/energy_polarization_multi.f90
+++ src/energy_polarization_multi.f90
@@ -1,34 +1,29 @@
 SUBROUTINE energy_polarization_multi (F_pol)
 
-    USE precision_kinds, ONLY: i2b , dp
-    USE system, ONLY: nfft1 , nfft2 , nfft3 , Lx , Ly , Lz , kBT , rho_0 , &
-                    deltav, molec_polarx_k,molec_polary_k, molec_polarz_k, kBT,&
-                    rho_0_multispec, nb_species, n_0, beta, spaceGrid, soluteSite
+    USE precision_kinds, ONLY: i2b, dp
+    USE system, ONLY: nfft1, nfft2, nfft3, kBT, rho_0, molec_polarx_k,molec_polary_k, molec_polarz_k, &
+                    nb_species, n_0, spaceGrid, soluteSite
     USE dcf, ONLY: chi_l, chi_t, c_s, nb_k, delta_k
-    use quadrature,only : Omx , Omy , Omz, sym_order , angGrid, molRotGrid
-    USE minimizer, ONLY: cg_vect , FF , dF
-    use constants,only : twopi, i_complex, fourpi, eps0,qunit,Navo, qfact
-    use fft,only : fftw3, kx, ky, kz, k2, norm_k
-    use input,only : input_line,input_log, input_char, verbose
+    USE quadrature,ONLY : angGrid, molRotGrid
+    USE minimizer, ONLY: cg_vect, FF, dF
+    USE constants, ONLY : twopi, fourpi, qfact
+    USE fft, ONLY: fftw3, kx, ky, kz, k2, norm_k
+    USE input, ONLY: verbose
 
     IMPLICIT NONE
 
     REAL(dp), INTENT(OUT) :: F_pol
-    REAL(dp) ::  deltaVk, F_pol_long, F_pol_trans , F_pol_tot  !Longitudinal , transverse and total Polarization free energy
-    REAL(dp) :: mu_SPCE, facsym
-    REAL(dp) :: time0, time1
-    REAL(dp), DIMENSION (nfft1, nfft2, nfft3,angGrid%n_angles,molRotGrid%n_angles,nb_species) ::dF_pol_long,dF_pol_trans,dF_pol_tot
-    COMPLEX(dp), DIMENSION (nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles, nb_species) :: dF_pol_long_k ,&
-                        dF_pol_trans_k, dF_pol_tot_k
+    REAL(dp) :: F_pol_long, F_pol_trans , F_pol_tot  !Longitudinal , transverse and total Polarization free energy
+    REAL(dp) :: mu_SPCE, facsym, deltaVk, time0, time1
+    REAL(dp), DIMENSION (nfft1, nfft2, nfft3,angGrid%n_angles,molRotGrid%n_angles,nb_species) ::dF_pol_tot,dF_pol_long,dF_pol_trans
+    COMPLEX(dp), DIMENSION (nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles, nb_species) :: &
+                                                                                dF_pol_long_k, dF_pol_trans_k, dF_pol_tot_k
     REAL(dp), DIMENSION (nfft1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles, nb_species) :: rho
     COMPLEX(dp), ALLOCATABLE, DIMENSION (:,:,:,:,:,:) :: rho_k
     COMPLEX(dp), ALLOCATABLE, DIMENSION (:,:,:,:) :: &
         P_trans_x_k, P_trans_y_k, P_trans_z_k, P_long_x_k, P_long_y_k, P_long_z_k, pola_tot_x_k, pola_tot_y_k, pola_tot_z_k
-    INTEGER(i2b) :: icg, i, j, k, o, p, n, s, k_index 
-
+    INTEGER(i2b) :: icg, i, j, k, o, p, n, s, k_index
     COMPLEX(dp) :: k_tens_k_Px, k_tens_k_Py, k_tens_k_Pz, toto
-    COMPLEX(dp) :: F_pol_long_k, F_pol_trans_k, F_pol_tot_k 
-    COMPLEX(dp), DIMENSION (nfft1/2+1, nfft2, nfft3) :: rho_c_k_myway
 
     IF (nb_species/=1) STOP 'transv_and_longi_polarization_micro IS NOT WORKING FOR MULTISPECIES'
 
@@ -42,15 +37,11 @@ SUBROUTINE energy_polarization_multi (F_pol)
 !                	Initialization				
 !            							
 !===================================================================================================================================
-    deltaVk=twopi**3/(Lx*Ly*Lz)
-
+    deltaVk = twopi**3/PRODUCT(spaceGrid%length)
+    F_pol = 0.0_dp
     F_pol_long = 0.0_dp
     F_pol_trans = 0.0_dp
     F_pol_tot = 0.0_dp
-    F_pol_long_k = (0.0_dp,0.0_dp)
-    F_pol_trans_k = (0.0_dp,0.0_dp)
-    F_pol_tot_k = (0.0_dp,0.0_dp)
-    F_pol = 0.0_dp
     dF_pol_long = 0.0_dp
     dF_pol_trans = 0.0_dp
     dF_pol_tot = 0.0_dp
@@ -58,7 +49,6 @@ SUBROUTINE energy_polarization_multi (F_pol)
     dF_pol_trans_k = (0.0_dp,0.0_dp)
     dF_pol_tot_k = (0.0_dp,0.0_dp)
     rho = 0.0_dp
-    rho_c_k_myway = (0._dp,0._dp)
 
     mu_SPCE=0.4238_dp*0.5773525_dp*2.0_dp !dipolar moment of SPCE water molecule in e.Angstromm
 !======================================================================================================
@@ -69,7 +59,7 @@ SUBROUTINE energy_polarization_multi (F_pol)
 !            ====================================================
 !Compute rho_k
     icg=0
-    DO s =1 , nb_species
+    DO s=1,SIZE(soluteSite)
         DO i=1,nfft1
             DO j=1, nfft2
                 DO k=1, nfft3
@@ -94,7 +84,7 @@ SUBROUTINE energy_polarization_multi (F_pol)
             DO p = 1, molRotGrid%n_angles
                 fftw3%in_forward = rho(:,:,:,o,p,s)
                 CALL dfftw_execute (fftw3%plan_forward)
-                rho_k(:,:,:,o,p,s) = fftw3%out_forward*deltaV
+                rho_k(:,:,:,o,p,s) = fftw3%out_forward*spacegrid%dv
             END DO
         END DO
     END DO
@@ -155,22 +145,23 @@ SUBROUTINE energy_polarization_multi (F_pol)
         k_index = INT ( norm_k(i,j,k) / delta_k ) + 1
         IF ( k_index > nb_k ) k_index = nb_k ! Here it happens that k_index gets higher than the highest c_k index. In this case one imposes k_index = k_index_max
         
-        F_pol_long_k = &
-            F_pol_long_k+deltaVk*fourpi*( P_long_x_k(i,j,k,s)*CONJG(P_long_x_k(i,j,k,s))&
+        F_pol_long = &
+            F_pol_long+deltaVk*fourpi*( P_long_x_k(i,j,k,s)*CONJG(P_long_x_k(i,j,k,s))&
                                         + P_long_y_k(i,j,k,s)*CONJG(P_long_y_k(i,j,k,s))&
                                         + P_long_z_k(i,j,k,s)*CONJG(P_long_z_k(i,j,k,s)))&
                                         /chi_l(k_index)*0.5_dp*qfact*rho_0**2*facsym/(twopi**3)
         
-        F_pol_trans_k = &
-            F_pol_trans_k+deltaVk*(P_trans_x_k(i,j,k,s)*CONJG(P_trans_x_k(i,j,k,s))&
+        F_pol_trans = &
+            F_pol_trans+deltaVk*(P_trans_x_k(i,j,k,s)*CONJG(P_trans_x_k(i,j,k,s))&
                                   +P_trans_y_k(i,j,k,s)*CONJG(P_trans_y_k(i,j,k,s))&
                                   +P_trans_z_k(i,j,k,s)*CONJG(P_trans_z_k(i,j,k,s)))&
                                   /chi_t(k_index)*0.5_dp*qfact*rho_0**2*facsym/(twopi**3)
         
-        F_pol_tot_k = &
-            F_pol_tot_k-kBT*3/(2*mu_SPCE**2*n_0)*deltaVk*facsym/(twopi**3)*rho_0**2*&
-            (pola_tot_x_k(i,j,k,s)*CONJG(pola_tot_x_k(i,j,k,s))+pola_tot_y_k(i,j,k,s)*&
-            CONJG(pola_tot_y_k(i,j,k,s))+pola_tot_z_k(i,j,k,s)*CONJG(pola_tot_z_k(i,j,k,s)))
+        F_pol_tot = &
+            F_pol_tot-kBT*3/(2*mu_SPCE**2*n_0)*deltaVk*facsym/(twopi**3)*rho_0**2*&
+            (pola_tot_x_k(i,j,k,s)*CONJG(pola_tot_x_k(i,j,k,s))&
+            +pola_tot_y_k(i,j,k,s)*CONJG(pola_tot_y_k(i,j,k,s))&
+            +pola_tot_z_k(i,j,k,s)*CONJG(pola_tot_z_k(i,j,k,s)))
         
         DO CONCURRENT ( o=1:angGrid%n_angles, p=1:molRotGrid%n_angles )
         
@@ -232,27 +223,13 @@ SUBROUTINE energy_polarization_multi (F_pol)
                     DO o=1,angGrid%n_angles
                         DO p=1, molRotGrid%n_angles
                             icg = icg + 1
-                            dF(icg)=dF(icg)+ dF_pol_tot (i,j,k,o,p,s)*cg_vect(icg)*rho_0*2.0_dp * deltav!* angGrid%weight(o) * molRotGrid%weight(p) 
+                            dF(icg)=dF(icg)+ dF_pol_tot (i,j,k,o,p,s)*cg_vect(icg)*rho_0*2.0_dp * spaceGrid%dv!* angGrid%weight(o) * molRotGrid%weight(p) 
                         END DO
                     END DO
                 END DO
             END DO
         END DO
     END DO
-!===================================================================================================================================
-!					Check if Polarization Free energy is Real
-!===================================================================================================================================
-    IF ( AIMAG(F_pol_tot_k)==0._dp .AND. AIMAG(F_pol_long_k)==0._dp .AND. AIMAG(F_pol_trans_k)==0._dp ) THEN
-        F_pol_tot   = REAL( F_pol_tot_k , dp)
-        F_pol_long  = REAL( F_pol_long_k,  dp)
-        F_pol_trans = REAL( F_pol_trans_k, dp)
-    ELSE
-        WRITE(*,*) 'Error in energy_polarization_myway Free energy is not Real'
-        WRITE(*,*) 'Imaginary part of F_pol_tot_k, F_pol_long_k and F_pol_trans_k:'
-        WRITE(*,*) AIMAG(F_pol_tot_k), AIMAG(F_pol_long_k), AIMAG(F_pol_trans_k)
-        STOP
-    END IF
-!===================================================================================================================================
 
     F_pol = F_pol_tot + F_pol_long + F_pol_trans
     FF = FF + F_pol
