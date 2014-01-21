be2e6c8f60d355141085347503d3cd9d78ec1573
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-01-21 19:11:27 +0100

    remove unused code to convolute density by gaussian to smooth

--- src/get_final_density.f90
+++ src/get_final_density.f90
@@ -2,74 +2,56 @@
 SUBROUTINE get_final_density ( neq )
 
     USE precision_kinds, ONLY: dp , i2b
-    USE system, ONLY : nb_species, spaceGrid
-    USE constants, ONLY : twopi
-    USE minimizer, ONLY: CG_vect
-    USE quadrature, ONLY : molRotSymOrder, angGrid, molRotGrid
-    USE fft, ONLY: fftw3 , timesExpPrefactork2
-    USE input, ONLY: verbose
+    USE system,          ONLY: nb_species, spaceGrid
+    USE constants,       ONLY: twopi
+    USE minimizer,       ONLY: CG_vect
+    USE quadrature,      ONLY: molRotSymOrder, angGrid, molRotGrid
+    USE fft,             ONLY: fftw3 , timesExpPrefactork2
+    USE input,           ONLY: verbose
     
     IMPLICIT NONE
 
+    INTEGER(i2b) :: i,j,k,omega,icg,s,p,nfft1,nfft2,nfft3
     REAL(dp), INTENT(OUT) :: neq (spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3),nb_species) ! equilibrium density(position)
-    INTEGER(i2b) :: i, j, k, omega, icg, species, p
-    REAL(dp) :: rho_over_fourpi, local_density
-    COMPLEX(dp), ALLOCATABLE, DIMENSION(:,:,:,:) :: rho_k 
-    REAL(dp) :: Nk ! Total number of k points = nfft1*nfft2*nfft3
-    INTEGER(i2b) :: nfft1, nfft2, nfft3
+    REAL(dp) :: rho,local_density,Nk
     REAL(dp), DIMENSION(3) :: dl
     REAL(dp), PARAMETER :: gaussianWidth = 0._dp
+    COMPLEX(dp), ALLOCATABLE, DIMENSION(:,:,:,:) :: rho_k 
     
     nfft1 = spaceGrid%n_nodes(1); nfft2 = spaceGrid%n_nodes(2); nfft3 = spaceGrid%n_nodes(3)
     dl = spaceGrid%dl
     Nk = real( sum( spaceGrid%n_nodes ), dp)
-        
+
     icg = 0
-    do species = 1 , nb_species
-        do i = 1 , nfft1 ; do j = 1 , nfft2 ; do k = 1 , nfft3
-            local_density = 0.0_dp
-            do omega = 1 , angGrid%n_angles
-                do p=1, molRotGrid%n_angles
-                    icg = icg + 1
-                    rho_over_fourpi = cg_vect ( icg ) ** 2 / (twopi**2*2.0_dp/molRotSymOrder)
-                    local_density = local_density + angGrid%weight ( omega ) *molRotGrid%weight(p)* rho_over_fourpi ! integral of rho over all orientations ie 'n'
+    DO s =1,nb_species
+        DO i =1,nfft1
+            DO j =1,nfft2
+                DO k =1,nfft3
+                    local_density = 0.0_dp
+                    DO omega =1,angGrid%n_angles
+                        DO p =1,molRotGrid%n_angles
+                            icg = icg + 1
+                            rho = cg_vect ( icg ) ** 2 / (twopi**2*2.0_dp/molRotSymOrder)
+                            local_density = local_density + angGrid%weight ( omega ) *molRotGrid%weight(p)* rho ! integral of rho over all orientations ie 'n'
+                        END DO
+                    END DO
+                    neq (i,j,k,s) = local_density
                 END DO
             END DO
-            neq (i,j,k,species) = local_density
-        END DO; END DO ; END DO
+        END DO
     END DO
 
-    IF ( gaussianWidth /= 0._dp ) THEN !convolute with a gaussian
-        ALLOCATE ( rho_k (nfft1/2+1, nfft2, nfft3, nb_species) )
-        DO species = 1, nb_species
-            fftw3%in_forward = neq ( : , : , : , species )
-            CALL dfftw_execute ( fftw3%plan_forward )
-            rho_k (:,:,:,species) = timesExpPrefactork2 ( fftw3%out_forward, gaussianWidth**2/2.0_dp )
-            fftw3%in_backward = rho_k (:,:,:,species)
-            DEALLOCATE( rho_k )
-            CALL dfftw_execute ( fftw3%plan_backward )
-            neq (:,:,:,species ) = fftw3%out_backward/Nk 
-        END DO
-    END IF
-
-    IF (verbose) THEN
-        open(11,file='output/density_along_x.dat')
-            do i=1,nfft1
-                write(11,*) (i-1)*dl(1), neq(i,nfft2/2+1,nfft3/2+1,1)
-            END DO
-        close(11)
-    
-        open(12,file='output/density_along_y.dat')
-            do i=1,nfft2
-                write(12,*) (i-1)*dl(2), neq(nfft1/2+1,i,nfft3/2+1,1)
-            END DO
-        close(12)
-    
-        open(13,file='output/density_along_z.dat')
-            do i=1,nfft3
-                write(13,*) (i-1)*dl(3), neq(nfft1/2+1,nfft2/2+1,i,1)
-            END DO
-        close(13)
-    END IF
+!~     IF ( gaussianWidth /= 0._dp ) THEN !convolute with a gaussian
+!~         ALLOCATE ( rho_k (nfft1/2+1, nfft2, nfft3, nb_species) )
+!~         DO s = 1, nb_species
+!~             fftw3%in_forward = neq ( : , : , : , s )
+!~             CALL dfftw_execute ( fftw3%plan_forward )
+!~             rho_k (:,:,:,s) = timesExpPrefactork2 ( fftw3%out_forward, gaussianWidth**2/2.0_dp )
+!~             fftw3%in_backward = rho_k (:,:,:,s)
+!~             DEALLOCATE( rho_k )
+!~             CALL dfftw_execute ( fftw3%plan_backward )
+!~             neq (:,:,:,s ) = fftw3%out_backward/Nk 
+!~         END DO
+!~     END IF
 
 END SUBROUTINE get_final_density
