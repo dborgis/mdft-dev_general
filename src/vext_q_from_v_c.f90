1c6d0374f6c34998662cd561ec176994d06f7062
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-01-21 19:33:20 +0100

    refrac and inline optimization

--- src/vext_q_from_v_c.f90
+++ src/vext_q_from_v_c.f90
@@ -16,17 +16,14 @@ SUBROUTINE vext_q_from_v_c (Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotz
 
     REAL(dp), DIMENSION ( angGrid%n_angles , molRotGrid%n_angles ), INTENT(IN) :: Rotxx , Rotxy , Rotxz , Rotyx , Rotyy , Rotyz , &
                                                                     Rotzx , Rotzy , Rotzz
-    INTEGER(i2b) :: i, j, k, o, p, m, z
+    INTEGER(i2b) :: i, j, k, o, p, m, z,s
     REAL(dp), DIMENSION ( nb_solvent_sites , molRotGrid%n_angles , angGrid%n_angles ) :: xmod , ymod , zmod
-    INTEGER(i2b):: species ! dummy
-    REAL(dp):: xq , yq , zq ! solvent coordinates in indices referential (ie real between 0 and nfft1+1)
+    REAL(dp):: xq,yq,zq ! solvent coordinates in indices referential (ie real between 0 and nfft1+1)
     INTEGER(i2b):: im , jm , km , ip , jp , kp ! 6 indices from which all corners of cube surrounding point charge are defined
     REAL(dp):: wim , wjm , wkm , wip , wjp , wkp ! weights associated to each 'corner' index
     REAL(dp):: vpsi ! external potential for a given i,j,k,omega & psi.
     REAL(dp):: average_over_psi ! boltzmann average of vpsi over psi for a given i,j,k,omega
-    REAL(dp):: time0, time1
-
-    CALL CPU_TIME( time0 )
+    REAL(dp):: charge
 
     z=0
     ! be sure this subroutine comes at right moment
@@ -34,19 +31,7 @@ SUBROUTINE vext_q_from_v_c (Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotz
     IF(.NOT. ALLOCATED(vext_q)) ALLOCATE( vext_q (spaceGrid%n_nodes(1), spaceGrid%n_nodes(2), spaceGrid%n_nodes(3),&
         angGrid%n_angles, molRotGrid%n_angles, nb_species ), SOURCE=0._dp )
     
-    IF ( ALL(soluteSite%q == zero) .OR. ALL(solventSite%q == zero) ) RETURN ! uncharged system
-
-    IF (verbose) THEN
-        OPEN(11, file='output/v_c.dat')
-            do i=1, nfft1
-                do j=1, nfft2
-                    do k=1, nfft3
-                        write(11,*) v_c(i , j , k)
-                    end do
-                end do
-            end do
-        CLOSE(11)
-    END IF
+    IF ( ALL(soluteSite%q == zero) .OR. ALL(solventSite%q == zero) ) RETURN
 
     ! Tabulate rotation matrix * solvent coordinates
     do o = 1 , angGrid%n_angles
@@ -60,99 +45,48 @@ SUBROUTINE vext_q_from_v_c (Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotz
     end do
     
     ! Compute external potential due to charge density
-    do species = 1 , nb_species
-        do i = 1 , nfft1
-        do j = 1 , nfft2
-        do k = 1 , nfft3
-            do o = 1 , angGrid%n_angles
-            average_over_psi = 0.0_dp ! init average over psi which is for all psi for a given i,j,k,omega
-            do p = 1 , molRotGrid%n_angles
-                vpsi = 0.0_dp ! vpsi is initiated. We make a boltzmann average over psi angles
-                do m = 1 , nb_solvent_sites
-                if ( chg_solv ( id_solv (m) ) == 0.0_dp ) cycle
-                xq = modulo ( real( i-1 ,dp) * deltax + xmod (m,p,o) , Lx ) / deltax
-                yq = modulo ( real( j-1 ,dp) * deltay + ymod (m,p,o) , Ly ) / deltay
-                zq = modulo ( real( k-1 ,dp) * deltaz + zmod (m,p,o) , Lz ) / deltaz
-                im = int ( xq ) + 1
-                jm = int ( yq ) + 1
-                km = int ( zq ) + 1
-                if ( im == nfft1 + 1 ) im = 1
-                if ( jm == nfft2 + 1 ) jm = 1
-                if ( km == nfft3 + 1 ) km = 1
-                ip = im + 1
-                jp = jm + 1
-                kp = km + 1
-                if ( ip == nfft1 + 1 ) ip = 1
-                if ( jp == nfft2 + 1 ) jp = 1
-                if ( kp == nfft3 + 1 ) kp = 1
-                wim = (    1.0_dp - (   xq - real( int( xq ,i2b),dp)   )    )
-                wjm = (    1.0_dp - (   yq - real( int( yq ,i2b),dp)   )    )
-                wkm = (    1.0_dp - (   zq - real( int( zq ,i2b),dp)   )    )
-                wip = (             (   xq - real( int( xq ,i2b),dp)   )    )
-                wjp = (             (   yq - real( int( yq ,i2b),dp)   )    )
-                wkp = (             (   zq - real( int( zq ,i2b),dp)   )    )
-                vpsi = vpsi + (chg_solv ( id_solv (m) ) * qfact * ( V_c (im,jm,km) * wim * wjm * wkm &
-                                                                + V_c (ip,jm,km) * wip * wjm * wkm &
-                                                                + V_c (im,jp,km) * wim * wjp * wkm &
-                                                                + V_c (im,jm,kp) * wim * wjm * wkp &
-                                                                + V_c (ip,jp,km) * wip * wjp * wkm &
-                                                                + V_c (ip,jm,kp) * wip * wjm * wkp &
-                                                                + V_c (im,jp,kp) * wim * wjp * wkp &
-                                                                + V_c (ip,jp,kp) * wip * wjp * wkp ))
-    !print*, species, p , o ,k ,j ,i, vpsi
-        !     print*, vpsi
-                end do ! solvent sites
-    !Potential depends of psi now
-    !            average_over_psi = average_over_psi + exp ( - beta * vpsi ) ! this is a boltzmann average over psi
-        !     write(*,*)  average_over_psi
-    
-            ! Things are clear but surely useless here.
-    !          if ( average_over_psi < 1.e-10_dp ) then
-    !            Vext_q ( i , j , k , o , species ) = 25.0_dp / beta ! TODO magic numbers are not welcome
-    !          else
-    !            Vext_q ( i , j , k , o , species ) = - log ( average_over_psi / molRotGrid%n_angles ) / beta
-    !          end if
-    !          if ( average_over_psi > 0.0_dp ) then
-                Vext_q ( i , j , k , o , p , species ) = vpsi !- log ( average_over_psi / molRotGrid%n_angles ) / (beta)
-    !          else
-    !            Vext_q ( i , j , k , o , species ) = huge(1.0_dp)
-    !          end if
-            if ( Vext_q ( i , j , k , o , p , species )        > 100.0_dp ) then
-                Vext_q ( i , j , k , o , p  , species )         = 100.0_dp
-            end if
-            
-            if ( Vext_q ( i , j , k , o , p , species )        < -100.0_dp ) then
-                Vext_q ( i , j , k , o , p , species )        = -100.0_dp
-            end if
-        
-            end do ! psi 
-    
-            end do ! omega
-        end do ! k
-        end do ! j
-    end do ! i
-    end do ! species
+    DO CONCURRENT( s=1:nb_species, i=1:nfft1, j=1:nfft2, k=1:nfft3, o=1:angGrid%n_angles, p=1:molRotGrid%n_angles )
+        vpsi = 0.0_dp
+        DO m = 1 , nb_solvent_sites
+            charge = chg_solv(id_solv(m))
+            IF ( charge==0.0_dp ) CYCLE
+            xq = MODULO( REAL(i-1,dp) * deltax + xmod(m,p,o) ,Lx)/deltax
+            yq = MODULO( REAL(j-1,dp) * deltay + ymod(m,p,o) ,Ly)/deltay
+            zq = MODULO( REAL(k-1,dp) * deltaz + zmod(m,p,o) ,Lz)/deltaz
+            im = INT(xq) +1
+            jm = INT(yq) +1
+            km = INT(zq) +1
+            IF ( im == nfft1 + 1 ) im = 1
+            IF ( jm == nfft2 + 1 ) jm = 1
+            IF ( km == nfft3 + 1 ) km = 1
+            ip = im + 1
+            jp = jm + 1
+            kp = km + 1
+            IF ( ip == nfft1 + 1 ) ip = 1
+            IF ( jp == nfft2 + 1 ) jp = 1
+            IF ( kp == nfft3 + 1 ) kp = 1
+            wim = (    1.0_dp - (   xq - real( int( xq ,i2b),dp)   )    )
+            wjm = (    1.0_dp - (   yq - real( int( yq ,i2b),dp)   )    )
+            wkm = (    1.0_dp - (   zq - real( int( zq ,i2b),dp)   )    )
+            wip = (             (   xq - real( int( xq ,i2b),dp)   )    )
+            wjp = (             (   yq - real( int( yq ,i2b),dp)   )    )
+            wkp = (             (   zq - real( int( zq ,i2b),dp)   )    )
+            vpsi = vpsi + (chg_solv ( id_solv (m) ) * qfact * ( V_c (im,jm,km) * wim * wjm * wkm &
+                                                              + V_c (ip,jm,km) * wip * wjm * wkm &
+                                                              + V_c (im,jp,km) * wim * wjp * wkm &
+                                                              + V_c (im,jm,kp) * wim * wjm * wkp &
+                                                              + V_c (ip,jp,km) * wip * wjp * wkm &
+                                                              + V_c (ip,jm,kp) * wip * wjm * wkp &
+                                                              + V_c (im,jp,kp) * wim * wjp * wkp &
+                                                              + V_c (ip,jp,kp) * wip * wjp * wkp ))
+        END DO
+        IF (vpsi>100._dp) THEN
+            Vext_q(i,j,k,o,p,s)=100._dp
+        ELSE IF (vpsi<-100_dp) THEN
+            Vext_q(i,j,k,o,p,s)=-100._dp
+        ELSE
+            Vext_q(i,j,k,o,p,s)=vpsi
+        END IF
+    END DO
 
-    call cpu_time(time1)
-    
-    ! Get the external potential over orientations and print it
-    !allocate ( temparray ( nfft1 , nfft2 , nfft3 ) )
-    !call mean_over_orientations ( Vext_q (:,:,:,:,1) , temparray )
-    !temparray = temparray / fourpi
-    !!filename = 'output/vext_q_from_v_c.cube'
-    !!call write_to_cube_file ( temparray , filename )
-    !filename = 'output/vext_q_from_v_c_along-z.dat'
-    !call compute_z_density ( temparray , filename )
-    !deallocate ( temparray )
-    ! DEBUG ONLY
-    
-    IF (verbose) THEN
-        PRINT*, 'vext_q_from_v_c : min = ' , minval ( Vext_q ) , ' ; max = ' , maxval ( Vext_q ) , ' ; in (sec) ' , time1 - time0
-        OPEN (10, FILE= 'output/vext_q_from_v_c_along-z_no_planar_average.dat' )
-            DO k=1,spaceGrid%n_nodes(3)
-                WRITE (10,*) (real(k-1,dp)*deltaz-Lz/2.0_dp) , (Vext_q(nfft1/2,nfft2/2,k,1,1,1) / QFACT)
-            END DO
-        CLOSE (10)
-    END IF
-    
 END SUBROUTINE vext_q_from_v_c
