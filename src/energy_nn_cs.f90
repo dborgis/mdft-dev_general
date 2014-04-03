9380a11c7110754b8c56489ac10c6d802f7d29af
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-04-03 13:31:05 +0200

    put things in subroutines to make code clearer

--- src/energy_nn_cs.f90
+++ src/energy_nn_cs.f90
@@ -3,12 +3,12 @@
 
 SUBROUTINE energy_nn_cs (Fint)
 
-    USE precision_kinds ,ONLY: i2b,dp
+    USE precision_kinds ,ONLY: i2b, dp
     USE system          ,ONLY: kBT, rho_0_multispec, spaceGrid
     USE dcf             ,ONLY: c_s, nb_k, delta_k
     USE quadrature      ,ONLY: molRotSymOrder, angGrid, molRotGrid
     USE minimizer       ,ONLY: cg_vect, FF, dF
-    USE constants       ,ONLY: fourpi, pi, twopi, zeroC
+    USE constants       ,ONLY: twopi, zeroC
     USE fft             ,ONLY: fftw3, norm_k
 
     IMPLICIT NONE
@@ -16,8 +16,9 @@ SUBROUTINE energy_nn_cs (Fint)
     REAL(dp), INTENT(OUT)    :: Fint
     INTEGER(i2b)             :: i,j,k,l,m,n,o,p,icg,species,nfft1,nfft2,nfft3,k_index,s,nspec
     REAL(dp)                 :: Vint, fact, psi, time1, time0
-    REAL(dp)   , ALLOCATABLE :: delta_rho(:,:,:), Vpair(:,:,:)
-    COMPLEX(dp), ALLOCATABLE :: delta_rho_k(:,:,:), Vpair_k(:,:,:)
+    REAL(dp)   , ALLOCATABLE :: delta_rho(:,:,:), gamma(:,:,:)
+    COMPLEX(dp), ALLOCATABLE :: delta_rho_k(:,:,:), gamma_k(:,:,:)
+    LOGICAL    , SAVE        :: c_s_isOK =.FALSE.
 
     nfft1 =spaceGrid%n_nodes(1)
     nfft2 =spaceGrid%n_nodes(2)
@@ -27,43 +28,24 @@ SUBROUTINE energy_nn_cs (Fint)
 
     ALLOCATE ( delta_rho(nfft1,nfft2,nfft3) ,SOURCE=0._dp )
 
-    ! Put density of last minimization step in delta_rho
-    icg=0
-    DO i=1,nfft1
-        DO j=1,nfft2
-            DO k=1,nfft3
-                psi = 0.0_dp
-                DO o = 1, angGrid%n_angles
-                    DO p=1 , molRotGrid%n_angles
-                        icg=icg+1
-                        psi = psi + angGrid%weight(o) * cg_vect(icg)**2*molRotGrid%weight(p)
-                    END DO
-                END DO
-                delta_rho(i,j,k) = psi
-            END DO
-        END DO
-    END DO
-    delta_rho = delta_rho -REAL(2.0_dp*twopi**2/molRotSymOrder, dp)
+    CALL fill_delta_rho_from_cgvect
     
-    ! deltarho in k-space
     fftw3%in_forward = delta_rho
     DEALLOCATE (delta_rho)
     CALL dfftw_execute ( fftw3%plan_forward )
-    ALLOCATE ( delta_rho_k (nfft1/2+1,nfft2,nfft3) ,SOURCE=fftw3%out_forward )
+    ALLOCATE ( delta_rho_k (nfft1/2+1,nfft2,nfft3) ,SOURCE=fftw3%out_forward ) ! deltarho in k-space
+    
+    CALL is_c_s_OK
     
     ! Polarisation in k-space
-    ALLOCATE ( Vpair_k (nfft1/2+1,nfft2,nfft3) ,SOURCE=zeroC)
-    IF (.NOT. ALLOCATED(c_s) .OR. ALL(c_s==0._dp)) STOP "I find c_s(k) to be unconsistent in energy_nn_cs.f90"
-    DO CONCURRENT (l=1:nfft1/2+1, m=1:nfft2, n=1:nfft3)
-        k_index =MIN(  INT(norm_k(l,m,n)/delta_k)+1  ,  nb_k  )
-        Vpair_k(l,m,n) = delta_rho_k(l,m,n) * c_s(k_index)     ! V(k)=cs(k)*rho(k)
-    END DO
+    ALLOCATE ( gamma_k (nfft1/2+1,nfft2,nfft3) ,SOURCE=zeroC)
+    CALL fill_gamma_in_kspace_ie_deltarho_times_c_s
     DEALLOCATE ( delta_rho_k )
 
-    fftw3%in_backward = Vpair_k
-    DEALLOCATE ( Vpair_k )
+    fftw3%in_backward = gamma_k
+    DEALLOCATE ( gamma_k )
     CALL dfftw_execute (fftw3%plan_backward)
-    ALLOCATE ( Vpair (nfft1,nfft2,nfft3) ,SOURCE=fftw3%out_backward/REAL(nfft1*nfft2*nfft3, dp))
+    ALLOCATE ( gamma (nfft1,nfft2,nfft3) ,SOURCE=fftw3%out_backward/REAL(nfft1*nfft2*nfft3, dp))
 
     ! excess free energy and its gradient
     Fint = 0.0_dp ! excess free energy
@@ -74,7 +56,7 @@ SUBROUTINE energy_nn_cs (Fint)
         DO i =1,nfft1
             DO j =1,nfft2
                 DO k =1,nfft3
-                    Vint = Vpair(i,j,k) * fact
+                    Vint = gamma(i,j,k) * fact
                     DO o =1,angGrid%n_angles
                         DO p =1,molRotGrid%n_angles
                             icg = icg + 1
@@ -87,9 +69,57 @@ SUBROUTINE energy_nn_cs (Fint)
             END DO
         END DO
     END DO
-    DEALLOCATE (Vpair)
+    DEALLOCATE (gamma)
 
     FF = FF + Fint
     CALL CPU_TIME (time1)
+
+ 
+    CONTAINS
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        SUBROUTINE is_c_s_OK
+            IF (.NOT. c_s_isOK) THEN
+                IF (.NOT. ALLOCATED(c_s) .OR. ALL(c_s==0._dp)) STOP "I find c_s(k) to be unconsistent in energy_nn_cs.f90"
+                c_s_isOK = .TRUE.
+            END IF
+        END SUBROUTINE is_c_s_OK
+    
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+        SUBROUTINE fill_delta_rho_from_cgvect
+            IMPLICIT NONE
+            REAL(dp)        :: psi
+            INTEGER(i2b)    :: icg,i,j,k,o,p
+            icg=0
+            DO i=1,nfft1
+                DO j=1,nfft2
+                    DO k=1,nfft3
+                        psi = 0.0_dp
+                        DO o=1,angGrid%n_angles
+                            DO p=1,molRotGrid%n_angles
+                                icg = icg+1
+                                psi = psi + angGrid%weight(o) * cg_vect(icg)**2*molRotGrid%weight(p)
+                            END DO
+                        END DO
+                        delta_rho(i,j,k) = psi
+                    END DO
+                END DO
+            END DO
+            delta_rho = delta_rho -REAL(2.0_dp*twopi**2/molRotSymOrder, dp)
+        END SUBROUTINE fill_delta_rho_from_cgvect
+    
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+        SUBROUTINE fill_gamma_in_kspace_ie_deltarho_times_c_s
+            INTEGER(i2b) :: l,m,n
+            DO CONCURRENT (l=1:nfft1/2+1, m=1:nfft2, n=1:nfft3)
+                k_index =MIN(  INT(norm_k(l,m,n)/delta_k)+1  ,  nb_k  )
+                gamma_k(l,m,n) = delta_rho_k(l,m,n) * c_s(k_index)     ! V(k)=cs(k)*rho(k)
+            END DO
+        END SUBROUTINE fill_gamma_in_kspace_ie_deltarho_times_c_s
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  
 END SUBROUTINE energy_nn_cs
