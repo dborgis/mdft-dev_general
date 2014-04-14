33a788894d0164887a92715833dfc897b51ed2ad
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-04-14 19:22:04 +0200

    complete object oriented refrac. More as an example for Lu than really usefull

--- src/energy_polarization_dipol.f90
+++ src/energy_polarization_dipol.f90
@@ -33,118 +33,14 @@ SUBROUTINE energy_polarization_dipol (Fint)
     Lx = spaceGrid%length(1); Ly = spaceGrid%length(2); Lz = spaceGrid%length(3)
     Nk = REAL ( nfft1*nfft2*nfft3 , dp ) ! total number of k grid points
     
-    ALLOCATE ( Px (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
-    ALLOCATE ( Py (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
-    ALLOCATE ( Pz (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
+    
+    CALL build_polarization_vector_field_Px_Py_Pz ! Px(r) = \int_\Omega \Omega(r) * \rho(r,\Omega) d\Omega  see Borgis et al., DOI:10.1103/PhysRevE.66.031206
+    CALL build_Fourier_transformed_polarization_vector_field_Pkx_Pky_Pkz
+    CALL build_excess_electric_field_in_Fourier_space
+    CALL build_excess_electric_field_from_inverse_Fourier_transform
 
-    ! put density of last minimization step in delta_rho and P
-    ! but first prepare the product angGrid%weight(omega)*Omx in order not to repeat it
-    ALLOCATE ( weight_omx ( angGrid%n_angles ), SOURCE=angGrid%weight*Omx )
-    ALLOCATE ( weight_omy ( angGrid%n_angles ), SOURCE=angGrid%weight*Omy )
-    ALLOCATE ( weight_omz ( angGrid%n_angles ), SOURCE=angGrid%weight*Omz )
-
-    icg = 0
-    DO i =1,nfft1
-        DO j =1,nfft2
-            DO k = 1,nfft3    
-                pxt = 0.0_dp
-                pyt = 0.0_dp
-                pzt = 0.0_dp
-                DO o = 1 , angGrid%n_angles
-                    DO p=1, molRotGrid%n_angles
-                        icg = icg + 1
-                        rho = cg_vect(icg) ** 2
-                        pxt = pxt + weight_Omx(o) * molRotGrid%weight(p) * rho
-                        pyt = pyt + weight_Omy(o) * molRotGrid%weight(p) * rho
-                        pzt = pzt + weight_Omz(o) * molRotGrid%weight(p) * rho
-                    END DO
-                END DO
-                Px (i,j,k) = pxt
-                Py (i,j,k) = pyt
-                Pz (i,j,k) = pzt
-            END DO
-        END DO
-    END DO
-
-    DEALLOCATE ( weight_omx, weight_omy, weight_omz )
-
-    IF (verbose) THEN
-        BLOCK
-            REAL(dp), DIMENSION (nfft1,nfft2,nfft3,1) ::  polatot
-!~             OPEN(11,FILE='output/polatotxmax')
-!~                 DO i=1,nfft1
-!~                     WRITE(11,*) i*dx, 0.4894_dp*Px(i,nfft2/2+1,nfft3/2+1)
-!~                 END DO
-!~             CLOSE(11)
-            !Compute Radial Polarization
-            filename='output/radial_polarization_dipolar'
-            polatot(:,:,:,1)=sqrt(0.4894_dp**2*(Px(:,:,:)**2+Py(:,:,:)**2+Pz(:,:,:)**2))*rho_0
-            CALL compute_rdf(polatot, filename)
-            filename='output/radial_polarization_scalar'
-        END BLOCK
-    END IF
-  
-    ! fourier transform px , py and pz
-    fftw3%in_forward = Px
-    DEALLOCATE ( Px )
-    CALL dfftw_execute ( fftw3%plan_forward )
-    ALLOCATE ( Pkx (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
-    
-    fftw3%in_forward = Py
-    DEALLOCATE ( Py )
-    CALL dfftw_execute ( fftw3%plan_forward )
-    ALLOCATE ( Pky (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
-    
-    fftw3%in_forward = Pz
-    DEALLOCATE ( Pz )
-    CALL dfftw_execute ( fftw3%plan_forward )
-    ALLOCATE ( Pkz (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
-    
-    
-    ! compute polarisation in k-space
-    ALLOCATE ( Ekx (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
-    ALLOCATE ( Eky (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
-    ALLOCATE ( Ekz (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
-    
-    ! get maximum number of k points as inputed in c_delta and c_d
-    DO CONCURRENT (l=1:nfft1/2+1, m=1:nfft2, n=1:nfft3)
-        pxt_k = Pkx(l,m,n)
-        pyt_k = Pky(l,m,n)
-        pzt_k = Pkz(l,m,n)
-        IF ( k2(l,m,n) /= 0.0_dp) THEN
-            k_dot_P = ( kproj(1,l)*pxt_k + kproj(2,m)*pyt_k + kproj(3,n)*pzt_k )/ k2(l,m,n)
-        ELSE
-            k_dot_P = CMPLX( tiny(1.0_dp),tiny(1.0_dp) ,dp )
-        END IF
-        k_index = MIN( INT( norm_k(l,m,n)/delta_k ) +1, nb_k)
-        c_deltat = c_delta ( k_index )
-        c_dt = c_d ( k_index )
-        Ekx ( l,m,n ) = c_deltat * pxt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(1,l) - pxt_k )
-        Eky ( l,m,n ) = c_deltat * pyt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(2,m) - pyt_k )
-        Ekz ( l,m,n ) = c_deltat * pzt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(3,n) - pzt_k )
-    END DO
-    
-    DEALLOCATE ( Pkx, Pky, Pkz )
-    
-    ! inverse fourier transform the polarization field
-    ! next inverse fourier transform sequence could be done in parallel
-    fftw3%in_backward = Ekx
-    DEALLOCATE (Ekx)
-    CALL dfftw_execute ( fftw3%plan_backward )
-    ALLOCATE ( Ex ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
-    
-    fftw3%in_backward = Eky
-    DEALLOCATE (Eky)
-    CALL dfftw_execute ( fftw3%plan_backward )
-    ALLOCATE ( Ey ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
-    
-    fftw3%in_backward = Ekz
-    DEALLOCATE (Ekz)
-    CALL dfftw_execute ( fftw3%plan_backward )
-    ALLOCATE ( Ez ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
     
     fact = spaceGrid%dv * rho_0! integration factor
-    
     Fint = 0.0_dp
     icg = 0
     DO i = 1, nfft1
@@ -171,5 +67,119 @@ SUBROUTINE energy_polarization_dipol (Fint)
     FF = FF + Fint
 
     DEALLOCATE ( Ex, Ey, Ez )
+
+
+
+    CONTAINS
+    
+        SUBROUTINE build_polarization_vector_field_Px_Py_Pz
+            ALLOCATE ( Px (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
+            ALLOCATE ( Py (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
+            ALLOCATE ( Pz (nfft1,nfft2,nfft3), SOURCE=0.0_dp )
+            ! put density of last minimization step in delta_rho and P
+            ! but first prepare the product angGrid%weight(omega)*Omx in order not to repeat it
+            ALLOCATE ( weight_omx ( angGrid%n_angles ), SOURCE=angGrid%weight*Omx )
+            ALLOCATE ( weight_omy ( angGrid%n_angles ), SOURCE=angGrid%weight*Omy )
+            ALLOCATE ( weight_omz ( angGrid%n_angles ), SOURCE=angGrid%weight*Omz )
+            icg = 0
+            DO i =1,nfft1
+                DO j =1,nfft2
+                    DO k = 1,nfft3    
+                        pxt = 0.0_dp
+                        pyt = 0.0_dp
+                        pzt = 0.0_dp
+                        DO o = 1 , angGrid%n_angles
+                            DO p=1, molRotGrid%n_angles
+                                icg = icg + 1
+                                rho = cg_vect(icg) ** 2
+                                pxt = pxt + weight_Omx(o) * molRotGrid%weight(p) * rho
+                                pyt = pyt + weight_Omy(o) * molRotGrid%weight(p) * rho
+                                pzt = pzt + weight_Omz(o) * molRotGrid%weight(p) * rho
+                            END DO
+                        END DO
+                        Px (i,j,k) = pxt
+                        Py (i,j,k) = pyt
+                        Pz (i,j,k) = pzt
+                    END DO
+                END DO
+            END DO
+            DEALLOCATE ( weight_omx, weight_omy, weight_omz )
+            IF (verbose) THEN
+                BLOCK
+                    REAL(dp), DIMENSION (nfft1,nfft2,nfft3,1) ::  polatot
+        !~             OPEN(11,FILE='output/polatotxmax')
+        !~                 DO i=1,nfft1
+        !~                     WRITE(11,*) i*dx, 0.4894_dp*Px(i,nfft2/2+1,nfft3/2+1)
+        !~                 END DO
+        !~             CLOSE(11)
+                    !Compute Radial Polarization
+                    filename='output/radial_polarization_dipolar'
+                    polatot(:,:,:,1)=sqrt(0.4894_dp**2*(Px(:,:,:)**2+Py(:,:,:)**2+Pz(:,:,:)**2))*rho_0
+                    CALL compute_rdf(polatot, filename)
+                    filename='output/radial_polarization_scalar'
+                END BLOCK
+            END IF
+        END SUBROUTINE build_polarization_vector_field_Px_Py_Pz
+        
+        SUBROUTINE build_Fourier_transformed_polarization_vector_field_Pkx_Pky_Pkz
+            ! fourier transform px , py and pz
+            fftw3%in_forward = Px
+            DEALLOCATE ( Px )
+            CALL dfftw_execute ( fftw3%plan_forward )
+            ALLOCATE ( Pkx (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
+            
+            fftw3%in_forward = Py
+            DEALLOCATE ( Py )
+            CALL dfftw_execute ( fftw3%plan_forward )
+            ALLOCATE ( Pky (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
+            
+            fftw3%in_forward = Pz
+            DEALLOCATE ( Pz )
+            CALL dfftw_execute ( fftw3%plan_forward )
+            ALLOCATE ( Pkz (nfft1/2+1, nfft2, nfft3) ,SOURCE=fftw3%out_forward)
+        END SUBROUTINE build_Fourier_transformed_polarization_vector_field_Pkx_Pky_Pkz
+        
+        SUBROUTINE build_excess_electric_field_in_Fourier_space
+            ! compute electric field in Fourier space
+            ALLOCATE ( Ekx (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
+            ALLOCATE ( Eky (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
+            ALLOCATE ( Ekz (nfft1/2+1, nfft2, nfft3) ,SOURCE=zeroC)
+            
+            ! get maximum number of k points as inputed in c_delta and c_d
+            DO CONCURRENT (l=1:nfft1/2+1, m=1:nfft2, n=1:nfft3)
+                pxt_k = Pkx(l,m,n)
+                pyt_k = Pky(l,m,n)
+                pzt_k = Pkz(l,m,n)
+                IF ( k2(l,m,n) /= 0.0_dp) THEN
+                    k_dot_P = ( kproj(1,l)*pxt_k + kproj(2,m)*pyt_k + kproj(3,n)*pzt_k )/ k2(l,m,n)
+                ELSE
+                    k_dot_P = CMPLX( tiny(1.0_dp),tiny(1.0_dp) ,dp )
+                END IF
+                k_index = MIN( INT( norm_k(l,m,n)/delta_k ) +1, nb_k)
+                c_deltat = c_delta ( k_index )
+                c_dt = c_d ( k_index )
+                Ekx ( l,m,n ) = c_deltat * pxt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(1,l) - pxt_k )
+                Eky ( l,m,n ) = c_deltat * pyt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(2,m) - pyt_k )
+                Ekz ( l,m,n ) = c_deltat * pzt_k + c_dt * ( 3.0_dp * k_dot_P * kproj(3,n) - pzt_k )
+            END DO
+            
+            DEALLOCATE ( Pkx, Pky, Pkz )
+        END SUBROUTINE build_excess_electric_field_in_Fourier_space
+        
+        SUBROUTINE build_excess_electric_field_from_inverse_Fourier_transform
+    ! inverse fourier transform the electric field
+    fftw3%in_backward = Ekx
+    DEALLOCATE (Ekx)
+    CALL dfftw_execute ( fftw3%plan_backward )
+    ALLOCATE ( Ex ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
+    fftw3%in_backward = Eky
+    DEALLOCATE (Eky)
+    CALL dfftw_execute ( fftw3%plan_backward )
+    ALLOCATE ( Ey ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
+    fftw3%in_backward = Ekz
+    DEALLOCATE (Ekz)
+    CALL dfftw_execute ( fftw3%plan_backward )
+    ALLOCATE ( Ez ( nfft1 , nfft2 , nfft3 ) ,SOURCE=fftw3%out_backward / Nk)
+END SUBROUTINE build_excess_electric_field_from_inverse_Fourier_transform
 
 END SUBROUTINE energy_polarization_dipol
