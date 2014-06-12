ca877491398d3953972b7860993eb49354ebb4f8
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-06-12 17:35:48 +0200

    rm unused and chop

--- src/compute_z_density.f90
+++ src/compute_z_density.f90
@@ -3,23 +3,20 @@
 SUBROUTINE compute_z_density (array,filename)
     
     USE precision_kinds ,ONLY: dp, i2b
-    USE system          ,ONLY: nfft1, nfft2, nfft3, spaceGrid
+    USE system          ,ONLY: spaceGrid
     USE mathematica     ,ONLY: chop
     
     IMPLICIT NONE
     
-    REAL(dp) :: mean_density
-    REAL(dp), DIMENSION (nfft1,nfft2,nfft3), INTENT(IN) :: array
+    REAL(dp) :: mean_density, z
+    REAL(dp), INTENT(IN) :: array(spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3))
     INTEGER(i2b) :: k
-    REAL(dp) :: z
     CHARACTER(50), INTENT(IN):: filename
-    
     OPEN (10, FILE=filename)
         ! Compute mean density over x and y
-        mean_density=0.0_dp
-        DO k=1,nfft3
+        DO k = 1, spaceGrid%n_nodes(3)
             z = REAL(k-1,dp) * spaceGrid%dl(3)
-            mean_density = chop(SUM(array(:,:,k)) / REAL(nfft1*nfft2,dp))
+            mean_density = chop(SUM(array(:,:,k)) / REAL(PRODUCT(spaceGrid%n_nodes(1:2)),dp))
             WRITE (10,*)z,mean_density
         END DO
     CLOSE (10)
