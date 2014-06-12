f59ff4a18d51216e19d2d7774d2a136a8a1007c3
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-06-12 14:37:47 +0200

    strong refrac + better error handling at reading input file

--- src/external_potential_hard_walls.f90
+++ src/external_potential_hard_walls.f90
@@ -1,3 +1,6 @@
+!===================================================================================================================================
+SUBROUTINE external_potential_hard_walls
+!===================================================================================================================================
 ! this SUBROUTINE computes the external potential created by several (0 to infty) hard walls
 ! The hard walls "plans" have coordinates read in input/dft.in
 ! For a brief description of the coordinates of a plan, see wikipedia: 
@@ -5,97 +8,80 @@
 ! Step 2/ we read their coordinates
 ! Step 3/ we read their thinkness (2*radius) in dft.in
 ! Step 4/ we compute the external potential created by the walls, which depends upon the fluid radius
-SUBROUTINE external_potential_hard_walls
-! precision_kinds defines the precision of the real, double, integer variables.
-USE precision_kinds,only : i2b , dp
-! i2b = integer simple precision_kind
-! dp = double precision real
-! contains input/dft.in put in input_line
-use input,only : input_line
-! input_line (:) contains all lines in dft.in
-use system,only : nfft1 , nfft2 , nfft3 , Lz , nb_species , deltax , deltay , deltaz
-! nfft1 = number of grid nodes in X direction
-! Lz = Length (Angstroms) of the supercell in X direction
-use external_potential,only : Vext_total
-use quadrature, only: angGrid, molRotGrid
-USE hardspheres ,ONLY: hs
-! Vext_total = external potential as used in the total free energy calculation
-IMPLICIT NONE
-integer(i2b):: i , j , k , wall ! dummy
-real(dp):: dplan ! local distance between point M (grid point) and plan defining wall
-integer(i2b):: species ! dummy between 1 and nb_species
-integer(i2b):: number_of_hard_walls ! number of hard walls in the supercell
-real (dp) , allocatable , dimension ( : ) :: thickness ! thickness of each wall (thickness = 2radius. Don't mix them up)
-real (dp) , allocatable , dimension ( : , : ) :: normal_vec ! normal vector of each plan defining wall
-real (dp) , allocatable , dimension ( : ) :: norm2_normal_vec ! norm of normal_vec
-real (dp) , allocatable , dimension ( : , : ) :: OA ! A is a point of coordinates OA(1,2,3) which is in the plan normal to normal_vec
-real (dp) , allocatable , dimension ( : ) :: dot_product_normal_vec_OA ! dummy
-real (dp) , dimension ( 3 ) :: OM ! coordinates of grid points
-real (dp) :: OMx , OMy , OMz ! projections of OM
-real (dp) , parameter :: infty = huge ( 1.0_dp )
-! read how many hard walls are wanted in the supercell
-do i = 1 , size ( input_line )
-  j = len ( 'hard_wall_number' )
-  if ( input_line (i) (1:j) == 'hard_wall_number' ) then
-    read ( input_line (i) (j+4:j+5) , * ) number_of_hard_walls
-    if ( number_of_hard_walls == 0 ) return ! no hard wall contribution to external potential. go back to init_external_potential
-!    call test_number_of_hard_walls ( number_of_hard_walls ) ! test if the total number of hard walls in the supercell is physical
-    allocate ( thickness ( number_of_hard_walls ) ) ! thickness of each wall (thickness = 2radius. Don't mix them up)
-    allocate ( normal_vec ( 3 , number_of_hard_walls ) ) ! normal vector of each plan defining wall
-    allocate ( norm2_normal_vec ( number_of_hard_walls ) ) ! norm of normal_vec. dummy
-    allocate ( OA ( 3 , number_of_hard_walls ) ) ! A is a point of coordinates OA(1,2,3) which is in the plan normal to normal_vec
-    allocate ( dot_product_normal_vec_OA ( number_of_hard_walls ) ) ! dummy
-    do wall = 1 , number_of_hard_walls
-      read ( input_line ( i + wall ) , * ) thickness ( wall ) , normal_vec ( 1 , wall ) , normal_vec ( 2 , wall ) , & 
-            normal_vec ( 3 , wall ) , OA ( 1 , wall ) , OA ( 2 , wall ) , OA ( 3 , wall ) ! for each wall, read thickness, normal vector coordinates and point in plan
-      norm2_normal_vec ( wall ) = norm2 ( normal_vec ( : , wall ) ) ! pretabulate norm of normal vec
-      dot_product_normal_vec_OA ( wall ) = dot_product ( - normal_vec ( : , wall ) , OA ( : , wall ) )
-    END DO
-    exit ! out of loop over i
-  END IF
-END DO
-! be sure Vext_total is allocated
-if ( .not. allocated ( Vext_total ) ) then
-  allocate ( Vext_total ( nfft1 , nfft2 , nfft3 , angGrid%n_angles , molRotGrid%n_angles , nb_species ) )
-  Vext_total = 0.0_dp
-END IF
-! be sure radius(:) (solvent radius) is already computed
-if ( .not. allocated ( hs ) ) then
-    write (*,*) 'radius is not allocated. critial stop in external_potential_hard_walls.f90'
-    STOP
-END IF
-! potential
-do k = 1 , nfft3
-  OMz = real ( k - 1 , dp ) * deltaz
-do j = 1 , nfft2
-  OMy = real ( j - 1 , dp ) * deltay
-do i = 1 , nfft1
-  OMx = real ( i - 1 , dp ) * deltax
-  OM = (/ OMx , OMy , OMz /)
-  do wall = 1 , number_of_hard_walls ! for each wall
-    dplan = abs ( dot_product ( normal_vec ( : , wall ) , OM ) + dot_product_normal_vec_OA ( wall ) ) / norm2_normal_vec ( wall ) ! compute distance between grid point and plan
-    do species = 1 , nb_species
-      if ( dplan <= 0.5_dp * thickness ( wall ) + hs(species)%R ) Vext_total ( i , j , k , : , : , species ) = infty
-    END DO ! species
-  END DO ! walls
-END DO
-END DO
-END DO
-contains
-!===================================================================================================================================
-! this SUBROUTINE tests if the number of hard walls is physical. For now, only 1 or 2 is implemented.
-!===================================================================================================================================
-  SUBROUTINE test_number_of_hard_walls ( number_of_hard_walls )
+
+    USE precision_kinds    ,ONLY: i2b, dp
+    USE input              ,ONLY: input_line, input_int
+    USE system             ,ONLY: nb_species, spaceGrid
+    USE external_potential ,ONLY: Vext_total
+    USE quadrature         ,ONLY: angGrid, molRotGrid
+    USE hardspheres        ,ONLY: hs
+
     IMPLICIT NONE
-    integer , intent(in) :: number_of_hard_walls
-    if ( number_of_hard_walls > 2 ) then
-      print *, 'the number of hard walls wanted by the user should not be > 2.'
-      print *, 'that is not implemented for now'
-      stop
-    ELSE IF ( number_of_hard_walls < 0 ) then
-      print *, 'number of hard walls is negative. That is non-sense.'
-      stop
+
+    INTEGER(i2b) :: i,j,k,w,s,nfft1,nfft2,nfft3,iostatint
+    INTEGER(i2b) :: species ! dummy between 1 and nb_species
+    INTEGER(i2b) :: nwall ! number of hard walls in the supercell
+    REAL(dp) :: dplan ! local distance between point M (grid point) and plan defining wall
+    REAL(dp), ALLOCATABLE, DIMENSION(:)   :: thickness ! thickness of each wall (thickness = 2radius. Don't mix them up)
+    REAL(dp), ALLOCATABLE, DIMENSION(:,:) :: normal_vec ! normal vector of each plan defining wall
+    REAL(dp), ALLOCATABLE, DIMENSION(:)   :: norm2_normal_vec ! norm of normal_vec
+    REAL(dp), ALLOCATABLE, DIMENSION(:,:) :: OA ! A is a point of coordinates OA(1,2,3) which is in the plan normal to normal_vec
+    REAL(dp), ALLOCATABLE, DIMENSION(:)   :: dot_product_normal_vec_OA ! dummy
+    REAL(dp) :: OM(3) ! coordinates of grid points
+
+    nfft1 = spaceGrid%n_nodes(1)
+    nfft2 = spaceGrid%n_nodes(2)
+    nfft3 = spaceGrid%n_nodes(3)
+
+! read how many hard walls are wanted in the supercell
+    DO i = 1, SIZE(input_line)
+        j = LEN('hard_wall_number')
+        IF ( input_line (i) (1:j) == 'hard_wall_number' ) THEN
+            READ ( input_line (i) (j+4:j+5) , * ) nwall
+            IF ( nwall == 0 ) RETURN ! no hard wall
+            ALLOCATE ( thickness (nwall) ,SOURCE=0._dp) ! thickness of each wall (thickness = 2radius. Don't mix them up)
+            ALLOCATE ( normal_vec (3,nwall) ,SOURCE=0._dp) ! normal vector of each plan defining wall
+            ALLOCATE ( norm2_normal_vec (nwall) ,SOURCE=0._dp) ! norm of normal_vec. dummy
+            ALLOCATE ( OA (3,nwall) ,SOURCE=0._dp) ! A is a point of coordinates OA(1,2,3) which is in the plan normal to normal_vec
+            ALLOCATE ( dot_product_normal_vec_OA (nwall) ,SOURCE=0._dp) ! dummy
+            DO w = 1 , nwall
+                READ ( input_line(i+w),*,IOSTAT=iostatint) thickness(w) , normal_vec(1:3,w) , OA (1:3,w) ! for each wall, read thickness, normal vector coordinates and point in plan
+                IF(iostatint/=0) STOP "I could not read line containing thickness of the hard wall, its normal vector and position"
+                norm2_normal_vec ( w ) = NORM2(normal_vec(:,w)) ! pretabulate norm of normal vec
+                dot_product_normal_vec_OA ( w ) = DOT_PRODUCT(-normal_vec(:,w) , OA(:,w))
+            END DO
+            EXIT
+        END IF
+    END DO
+
+! be sure Vext_total is allocated
+    IF (.NOT. ALLOCATED(Vext_total)) THEN
+        ALLOCATE( Vext_total ( nfft1 , nfft2 , nfft3 , angGrid%n_angles , molRotGrid%n_angles , nb_species ) ,SOURCE=0._dp)
     END IF
-  END SUBROUTINE test_number_of_hard_walls
-!===================================================================================================================================
+! be sure radius(:) (solvent radius) is already computed
+    IF(.NOT. ALLOCATED(hs)) STOP 'radius is not allocated. critial stop in external_potential_hard_walls.f90'
+    
+    ! compute the potential potential
+!~     DO k=1,nfft3
+!~         OMz = REAL(k-1,dp)*spaceGrid%dl(3)
+!~         DO j=1,nfft2
+!~             OMy = REAL(j-1,dp)*spaceGrid%dl(2)
+!~             DO i = 1 , nfft1
+!~                 OMx = REAL(i-1,dp)*spaceGrid%dl(1)
+!~                 OM = (/ OMx , OMy , OMz /)
+!~                 DO w = 1 , nwall ! for each wall
+!~                     dplan = ABS( DOT_PRODUCT( normal_vec(:,w),OM ) + dot_product_normal_vec_OA(w) )/ norm2_normal_vec(w) ! compute distance between grid point and plan
+!~                     DO s = 1 , nb_species
+!~                         if ( dplan <= 0.5_dp*thickness(w) + hs(s)%R ) Vext_total(i,j,k,:,:,s)=infty
+!~                     END DO
+!~                 END DO
+!~             END DO
+!~         END DO
+!~     END DO
+    DO CONCURRENT ( i=1:nfft1, j=1:nfft2, k=1:nfft3, s=1:nb_species, w=1:nwall)
+        OM = [ REAL(i-1,dp)*spaceGrid%dl(1) , REAL(j-1,dp)*spaceGrid%dl(2) , REAL(k-1,dp)*spaceGrid%dl(3) ]
+        dplan = ABS( DOT_PRODUCT( normal_vec(:,w),OM ) + dot_product_normal_vec_OA(w) )/ norm2_normal_vec(w) ! compute distance between grid point and plan
+        IF ( dplan <= 0.5_dp*thickness(w) + hs(s)%R ) Vext_total(i,j,k,:,:,s) = HUGE(1.0_dp)
+    END DO
+
 END SUBROUTINE external_potential_hard_walls
