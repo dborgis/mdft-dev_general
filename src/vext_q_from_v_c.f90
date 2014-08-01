2e78def34aca208bd468f378ea61ec12f7b731ae
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-08-01 17:51:39 +0200

    Smarter rewritting of the interpolation of the Poisson potential to any solvent site coordinate

--- src/vext_q_from_v_c.f90
+++ src/vext_q_from_v_c.f90
@@ -8,12 +8,13 @@ SUBROUTINE vext_q_from_v_c (gridnode, Vpoisson)
     USE external_potential  ,ONLY: vext_q
     USE constants, ONLY: fourpi, qfact, qunit, zero
     USE input, ONLY: verbose
+    USE mathematica ,ONLY: TriLinearInterpolation
 
     IMPLICIT NONE
 
     INTEGER(i2b), INTENT(IN) :: gridnode(3)
     REAL(dp), DIMENSION(gridnode(1),gridnode(2),gridnode(3)), INTENT(IN) :: Vpoisson
-    INTEGER(i2b) :: i, j, k, o, p, m, s, nfft1, nfft2, nfft3
+    INTEGER(i2b) :: i, j, k, o, p, m, s, nfft(3), l(3), u(3)
     REAL(dp), DIMENSION ( nb_solvent_sites , molRotGrid%n_angles , angGrid%n_angles ) :: xmod , ymod , zmod
     REAL(dp):: xq,yq,zq ! solvent coordinates in indices referential (ie real between 0 and nfft1+1)
     INTEGER(i2b):: im , jm , km , ip , jp , kp ! 6 indices from which all corners of cube surrounding point charge are defined
@@ -21,10 +22,10 @@ SUBROUTINE vext_q_from_v_c (gridnode, Vpoisson)
     REAL(dp):: vpsi ! external potential for a given i,j,k,omega & psi.
     REAL(dp):: average_over_psi ! boltzmann average of vpsi over psi for a given i,j,k,omega
     REAL(dp):: charge
+    real(dp) :: r(3), cube(0:1,0:1,0:1), dl(3)
 
-    nfft1=spaceGrid%n_nodes(1)
-    nfft2=spaceGrid%n_nodes(2)
-    nfft3=spaceGrid%n_nodes(3)
+    nfft=spaceGrid%n_nodes
+    dl = spaceGrid%dl
 
     IF(.NOT. ALLOCATED(vext_q)) STOP "vext_q should already be allocated in vext_q_from_v_c.f90"
     IF( ANY(vext_q/=0._dp) ) STOP "vext_q should be zero everywhere in vext_q_from_v_c.f90"
@@ -38,43 +39,36 @@ SUBROUTINE vext_q_from_v_c (gridnode, Vpoisson)
         zmod(m,p,o) = Rotzx(o,p) * solventSite(m)%r(1) + Rotzy(o,p) * solventSite(m)%r(2) + Rotzz(o,p) * solventSite(m)%r(3)
     END DO
 
-    ! Compute external potential due to charge density
-    DO CONCURRENT( s=1:nb_species, i=1:nfft1, j=1:nfft2, k=1:nfft3, o=1:angGrid%n_angles, p=1:molRotGrid%n_angles )
+
+    ! Compute external potential for each combination of solvent side and grid node and orientation
+    DO CONCURRENT( s=1:nb_species, i=1:nfft(1), j=1:nfft(2), k=1:nfft(3), o=1:angGrid%n_angles, p=1:molRotGrid%n_angles )
         vpsi = 0.0_dp
         DO CONCURRENT (m=1:nb_solvent_sites, solventSite(m)%q/=0._dp)
-            xq = MODULO( REAL(i-1,dp) * spaceGrid%dl(1) + xmod(m,p,o) ,spaceGrid%length(1)) /spaceGrid%dl(1)
-            yq = MODULO( REAL(j-1,dp) * spaceGrid%dl(2) + ymod(m,p,o) ,spaceGrid%length(2)) /spaceGrid%dl(2)
-            zq = MODULO( REAL(k-1,dp) * spaceGrid%dl(3) + zmod(m,p,o) ,spaceGrid%length(3)) /spaceGrid%dl(3)
-            im = INT(xq) +1
-            jm = INT(yq) +1
-            km = INT(zq) +1
-            IF ( im == nfft1 + 1 ) im = 1
-            IF ( jm == nfft2 + 1 ) jm = 1
-            IF ( km == nfft3 + 1 ) km = 1
-            ip = im + 1
-            jp = jm + 1
-            kp = km + 1
-            IF ( ip == nfft1 + 1 ) ip = 1
-            IF ( jp == nfft2 + 1 ) jp = 1
-            IF ( kp == nfft3 + 1 ) kp = 1
-            wip = xq - REAL(INT( xq ,i2b),dp)
-            wjp = yq - REAL(INT( yq ,i2b),dp)
-            wkp = zq - REAL(INT( zq ,i2b),dp)
-            wim = 1.0_dp - wip
-            wjm = 1.0_dp - wjp
-            wkm = 1.0_dp - wkp
-            vpsi = vpsi + solventSite(m)%q * (  Vpoisson (im,jm,km) * wim * wjm * wkm &
-                                              + Vpoisson (ip,jm,km) * wip * wjm * wkm &
-                                              + Vpoisson (im,jp,km) * wim * wjp * wkm &
-                                              + Vpoisson (im,jm,kp) * wim * wjm * wkp &
-                                              + Vpoisson (ip,jp,km) * wip * wjp * wkm &
-                                              + Vpoisson (ip,jm,kp) * wip * wjm * wkp &
-                                              + Vpoisson (im,jp,kp) * wim * wjp * wkp &
-                                              + Vpoisson (ip,jp,kp) * wip * wjp * wkp )
+            r = real([i,j,k],dp)
+            r = (r-1.0_dp)*dl + [xmod(m,p,o),ymod(m,p,o),zmod(m,p,o)]! cartesian coordinate x of the solvent site m. May be outside the supercell.
+            r = r/dl ! cartesian coordinates in index scale
+            l = floor(r) ! index of node just below
+            u = l+1
+            r = r-real(l,dp)  ! cartesian coordinates between 0 and 1
+            l = modulo(l,nfft)+1
+            u = modulo(u,nfft)+1
+!~             if( any(r<0._dp) .or. any(r>1._dp) ) stop "problem with r"
+!~             if( any(l<1) .or. any(l>nfft) ) stop "problem with l"
+!~             if( any(u<1) .or. any(u>nfft) ) stop "problem with u"
+            cube(0,0,0) = Vpoisson (l(1),l(2),l(3))
+            cube(1,0,0) = Vpoisson (u(1),l(2),l(3))
+            cube(0,1,0) = Vpoisson (l(1),u(2),l(3))
+            cube(0,0,1) = Vpoisson (l(1),l(2),u(3))
+            cube(1,1,0) = Vpoisson (u(1),u(2),l(3))
+            cube(1,0,1) = Vpoisson (u(1),l(2),u(3))
+            cube(0,1,1) = Vpoisson (l(1),u(2),u(3))
+            cube(1,1,1) = Vpoisson (u(1),u(2),u(3))
+            vpsi = vpsi + solventSite(m)%q * TriLinearInterpolation(cube,r)
         END DO
         vpsi = qfact * vpsi
         CALL limit_potential(vpsi)
         Vext_q(i,j,k,o,p,s) = vpsi
+
     END DO
 
     CONTAINS
