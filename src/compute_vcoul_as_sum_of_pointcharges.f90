01838c0a854140ffd42f3f2343e38fe8c6b45ce9
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-08-04 23:27:01 +0200

    Stop using x_solv. Use correct type.

--- src/compute_vcoul_as_sum_of_pointcharges.f90
+++ src/compute_vcoul_as_sum_of_pointcharges.f90
@@ -5,8 +5,8 @@
 SUBROUTINE compute_vcoul_as_sum_of_pointcharges
 
     USE precision_kinds, only: dp,i2b
-    use system, only: nfft1,nfft2,nfft3,deltax,deltay,deltaz,id_solv,id_mol,x_solv,y_solv,z_solv,x_mol,y_mol,z_mol,&
-                        beta,nb_solute_sites,nb_solvent_sites,chg_mol,chg_solv,Lx,Ly,Lz , nb_species
+    use system, only: nfft1,nfft2,nfft3,deltax,deltay,deltaz,id_solv,id_mol,x_mol,y_mol,z_mol,&
+                        beta,nb_solute_sites,nb_solvent_sites,chg_mol,chg_solv,Lx,Ly,Lz , nb_species, solventSite, soluteSite
     use constants,only : fourpi , qfact
     use external_potential,only : Vext_q
     use quadrature, only: angGrid, molRotGrid, Rotxx, Rotxy, Rotxz, Rotyx, Rotyy, Rotyz, Rotzx, Rotzy, Rotzz
@@ -14,7 +14,7 @@ SUBROUTINE compute_vcoul_as_sum_of_pointcharges
 
     IMPLICIT NONE
 
-    integer(i2b) :: i,j,k,o,p,m,n ! dummy
+    integer(i2b) :: i,j,k,o,p,m,n,s
     real(dp),dimension(nb_solvent_sites,molRotGrid%n_angles,angGrid%n_angles) :: xmod,ymod,zmod
     real(dp) :: x_grid,y_grid,z_grid ! coordinates of grid nodes
     real(dp) :: x_m,y_m,z_m ! solvent sites coordinates
@@ -27,7 +27,6 @@ SUBROUTINE compute_vcoul_as_sum_of_pointcharges
     character(50):: filename
     real(dp) :: qfactcc ! qfact*chg_solv()*chg_mol()
     real(dp) :: tempVcoul ! temporary Vcoul(i,j,k,o)
-    integer(i2b):: species ! dummy for loops over species
     REAL, PARAMETER :: Rc=1.0_dp, Rc2=Rc**2 ! charge pseudo radius **2   == Rc**2
 
 
@@ -47,14 +46,14 @@ SUBROUTINE compute_vcoul_as_sum_of_pointcharges
     do o=1,angGrid%n_angles
         do p=1,molRotGrid%n_angles
             do m=1,nb_solvent_sites
-                xmod(m,p,o)= Rotxx(o,p)*x_solv(m) + Rotxy(o,p)*y_solv(m) + Rotxz(o,p)*z_solv(m)
-                ymod(m,p,o)= Rotyx(o,p)*x_solv(m) + Rotyy(o,p)*y_solv(m) + Rotyz(o,p)*z_solv(m)
-                zmod(m,p,o)= Rotzx(o,p)*x_solv(m) + Rotzy(o,p)*y_solv(m) + Rotzz(o,p)*z_solv(m)
+                xmod(m,p,o)= Rotxx(o,p)*solventSite(m)%r(1) + Rotxy(o,p)*solventSite(m)%r(2) + Rotxz(o,p)*solventSite(m)%r(3)
+                ymod(m,p,o)= Rotyx(o,p)*solventSite(m)%r(1) + Rotyy(o,p)*solventSite(m)%r(2) + Rotyz(o,p)*solventSite(m)%r(3)
+                zmod(m,p,o)= Rotzx(o,p)*solventSite(m)%r(1) + Rotzy(o,p)*solventSite(m)%r(2) + Rotzz(o,p)*solventSite(m)%r(3)
             END DO
         END DO
     END DO
 
-    do species = 1 , nb_species
+    do s = 1 , nb_species
         do k=1,nfft3
             z_grid=real(k-1,dp)*deltaz
             do j=1,nfft2
@@ -65,16 +64,14 @@ SUBROUTINE compute_vcoul_as_sum_of_pointcharges
                         tempVcoul=0.0_dp
                         ploop:  do p=1,molRotGrid%n_angles
                             V_psi=0.0_dp
-                            do m=1,nb_solvent_sites
-                                ids=id_solv(m)
-                                if (chg_solv(ids)==0.0_dp) cycle
+                            do m=1,SIZE(solventSite)
+                                if (solventSite(m)%q==0.0_dp) cycle
                                 x_m = x_grid + xmod(m,p,o)
                                 y_m = y_grid + ymod(m,p,o)
                                 z_m = z_grid + zmod(m,p,o)
-                                do n=1,nb_solute_sites
-                                    idm=id_mol(n)
-                                    if (chg_mol(idm)==0.0_dp) cycle
-                                    qfactcc=qfact*chg_solv(ids)*chg_mol(idm)
+                                do n=1,SIZE(soluteSite)
+                                    if (soluteSite(n)%q==0.0_dp) cycle
+                                    qfactcc=qfact*solventSite(m)%q*soluteSite(n)%q
                                     x_nm = x_m - x_mol(n)
                                     y_nm = y_m - y_mol(n)
                                     z_nm = z_m - z_mol(n)
@@ -89,12 +86,12 @@ SUBROUTINE compute_vcoul_as_sum_of_pointcharges
                             END DO ! solvent
                         !tempVcoul=tempVcoul+exp(-beta*V_psi)
                         !if (tempVcoul<1.e-10_dp) then
-                        Vext_q(i,j,k,o,p,species ) = V_psi
+                        Vext_q(i,j,k,o,p,s ) = V_psi
                         !ELSE
                         !   Vext_q(i,j,k,o,1)=-log(tempVcoul/molRotGrid%n_angles)/beta
                         !END IF
                         ! Limit maximum value
-                        if (Vext_q(i,j,k,o,p,species)>100.0_dp) Vext_q(i,j,k,o,p,species)=100.0_dp
+                        if (Vext_q(i,j,k,o,p,s)>100.0_dp) Vext_q(i,j,k,o,p,s)=100.0_dp
                         !!!!!        if (Vcoul(i,j,k,o)<=-10.0_dp) then
                         !!!!!          write(*,*)'problem in compute_vcoul_ijko_from_tabulated.f90'
                         !!!!!          write(*,*)'reduced coordinate of problematic grid point : ',x_grid/Lx,y_grid/Ly,z_grid/Lz
