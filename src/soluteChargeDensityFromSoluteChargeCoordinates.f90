0bc1bbabf302b01afa66a92b05f032bd3579d182
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-08-04 16:38:22 +0200

    Bug volume element is the one of Poisson grid, not of MDFT's grid. For now, both are the same so this bug did not have any consequence.

--- src/soluteChargeDensityFromSoluteChargeCoordinates.f90
+++ src/soluteChargeDensityFromSoluteChargeCoordinates.f90
@@ -14,6 +14,7 @@ SUBROUTINE soluteChargeDensityFromSoluteChargeCoordinates (gridnode, gridlen, so
     REAL(dp), INTENT(IN) :: gridlen(3)
     REAL(dp), DIMENSION(gridnode(1),gridnode(2),gridnode(3)), INTENT(OUT) :: soluteChargeDensity
     INTEGER(i2b):: s, m(3), p(3)
+    REAL(dp) :: volumElem ! elementary volume in Poisson Grid space
     REAL(dp) :: r(3) ! coordinates of the charge in indicial coordinates
     REAL(dp) :: wm(3), wp(3) ! weight associated to each index
     CHARACTER(50) :: filename
@@ -42,7 +43,10 @@ SUBROUTINE soluteChargeDensityFromSoluteChargeCoordinates (gridnode, gridlen, so
         soluteChargeDensity (m(1),p(2),p(3)) = soluteChargeDensity (m(1),p(2),p(3)) + soluteSite(s)%q * wm(1) * wp(2) * wp(3)
         soluteChargeDensity (p(1),p(2),p(3)) = soluteChargeDensity (p(1),p(2),p(3)) + soluteSite(s)%q * wp(1) * wp(2) * wp(3)
     END DO
-    soluteChargeDensity = soluteChargeDensity / spaceGrid%dv ! charge density is in charge per unit volume
+    
+    volumElem = PRODUCT(gridlen/REAL(gridnode,dp))
+    soluteChargeDensity = soluteChargeDensity / volumElem ! charge density is in charge per unit volume
+
 
     IF (verbose) THEN
         filename='output/soluteChargeDensity.cube'
