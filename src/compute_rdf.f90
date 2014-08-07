c07f90f05f96fa7fd436fa1d1c315bf24bcb264e
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-08-07 18:44:01 +0200

    New dynamic scheme + Unit Testing

--- src/compute_rdf.f90
+++ src/compute_rdf.f90
@@ -16,72 +16,118 @@ SUBROUTINE compute_rdf (array,filename)
     REAL(dp), DIMENSION(spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3),nb_species), INTENT(IN) :: array
     CHARACTER(50), INTENT(IN) :: filename
     REAL(dp) :: RdfMaxRange, dr, r
-    REAL(dp), ALLOCATABLE :: rdf(:,:)
-    INTEGER(i2b), ALLOCATABLE :: recurrence_bin(:,:)
-    INTEGER(i2b):: i,j,k,n,bin,s,nbins
+    REAL(dp), ALLOCATABLE :: rdf(:)
+    INTEGER(i2b):: n,bin,nbins
     TYPE :: errortype
         LOGICAL :: found
         CHARACTER(180) :: msg
     END TYPE
     TYPE (errortype) :: error
     
-    nbins = CEILING (2._sp*REAL (PRODUCT (spaceGrid%n_nodes), sp)**(1._sp/3._sp)) ! Rice Rule, see http://en.wikipedia.org/wiki/Histogram
-    RdfMaxRange= SQRT(3._dp)*MINVAL(spaceGrid%length)/2._dp
-    dr = RdfMaxRange/REAL(nbins,dp) ! Width of each bin of the histogram
-
-    OPEN(10,FILE=filename,FORM='formatted')
-    WRITE(10,*)'# r  rdf'
-
-    ALLOCATE( recurrence_bin ( SIZE(soluteSite), nbins) ,SOURCE=0)
-    ALLOCATE( rdf            ( SIZE(soluteSite), nbins) ,SOURCE=0._dp)
-
-    error%found = .FALSE.
+    CALL deduce_optimal_histogram_properties
     
+    OPEN (10, FILE=filename, FORM='formatted')
+    WRITE (10,*) '# r  rdf'
+
+    error%found = .FALSE.    
     IF (nb_species/=1) STOP "compute_rdf.f90 is written for 1 solvent species only."
 
-    recurrence_bin = 0
-    rdf = 0.0_dp
-    WRITE(10,*)'# You want',s,'molecular solvents'
-
-    ! Transform array(position) in rdf(radialdistance)
-    ! counts the total number of appearence of a value in each bin
-    DO CONCURRENT (n=1:SIZE(soluteSite), i=1:spaceGrid%n_nodes(1), j=1:spaceGrid%n_nodes(2), k=1:spaceGrid%n_nodes(3))
-        r = NORM2(REAL([i,j,k]-1,dp)*spaceGrid%dl - soluteSite(n)%r)
-        bin = INT(r/dr)+1
-        IF (bin>nbins) THEN
-            CYCLE
-        ELSE IF (bin<=0) THEN
-            error%found = .TRUE.
-            error%msg = "I found a null or negative bin index in compute_rdf.f90.\\ STOP"
-        END IF
-        recurrence_bin(n,bin) =recurrence_bin(n,bin) +1
-        rdf           (n,bin) =rdf(n,bin) +array(i,j,k,s)
-    END DO
-
-    IF (error%found) THEN
-        PRINT*,error%msg
-        STOP
-    END IF
-
-    !normalize the rdf
-    WHERE (recurrence_bin/=0)
-        rdf = rdf/REAL(recurrence_bin,dp)
-    ELSEWHERE
-        rdf = 0.0_dp
-    END WHERE
-
-    ! Write rdf
-    DO n=1,SIZE(soluteSite)
-        WRITE(10,*)'#site ' , n
-        WRITE(10,*)0._dp,0._dp
+    ALLOCATE (rdf (nbins) ,SOURCE=0._dp)
+    
+    CALL UTest_histogram_3D
+    STOP "AFTER it works"
+    DO n = 1, SIZE(soluteSite) ! loop over all sites of the solute
+        CALL histogram_3d (array(:,:,:,1), rdf)
+        IF (error%found) THEN; PRINT*,error%msg; STOP; END IF
+        ! Write to output/rdf.out
+        WRITE(10,*)'# solute site', n
+        WRITE(10,*) 0._dp,0._dp ! we impose
         DO bin =1,nbins
-            WRITE(10,*) (REAL(bin,dp)-0.5_dp)*dr, rdf(n,bin) ! For bin that covers 0<r<dr, I print at 0.5dr, i.e., at the middle of the bin
+            WRITE(10,*) (REAL(bin,dp)-0.5_dp)*dr, rdf(bin) ! For bin that covers 0<r<dr, I print at 0.5dr, i.e., at the middle of the bin
         END DO
         WRITE(10,*)
     END DO
-    WRITE(10,*)! white line between solvent molecules
-
-    DEALLOCATE (recurrence_bin, rdf)
+    DEALLOCATE (rdf)
     CLOSE(10)
+
+    
+    CONTAINS
+
+        !===========================================================================================================================
+        SUBROUTINE deduce_optimal_histogram_properties
+        !===========================================================================================================================
+            nbins = CEILING (2._sp*REAL (PRODUCT (spaceGrid%n_nodes), sp)**(1._sp/3._sp)) ! Rice Rule, see http://en.wikipedia.org/wiki/Histogram
+            RdfMaxRange= SQRT(3._dp)*MINVAL(spaceGrid%length)/2._dp
+            dr = RdfMaxRange/REAL(nbins,dp) ! Width of each bin of the histogram
+        END SUBROUTINE deduce_optimal_histogram_properties
+        !===========================================================================================================================
+    
+        !===========================================================================================================================
+        SUBROUTINE histogram_3d (array3D, rdf)
+        !===========================================================================================================================
+            REAL(dp), INTENT(IN) :: array3D(spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3))
+            REAL(dp), INTENT(OUT) :: rdf(nbins)
+            INTEGER(i2b), ALLOCATABLE :: recurrence_bin(:)
+            REAL(dp) :: r
+            INTEGER(i2b) :: bin, i, j, k
+            
+            ALLOCATE( recurrence_bin (nbins) ,SOURCE=0)
+            rdf = 0._dp
+            
+            ! Transform array(position) in rdf(radialdistance)
+            ! counts the total number of appearence of a value in each bin
+            DO CONCURRENT (i=1:spaceGrid%n_nodes(1), j=1:spaceGrid%n_nodes(2), k=1:spaceGrid%n_nodes(3))
+                r = NORM2(REAL([i,j,k]-1,dp)*spaceGrid%dl - soluteSite(n)%r)
+                bin = INT(r/dr)+1
+                IF (bin>nbins) THEN
+                    CYCLE
+                ELSE IF (bin<=0) THEN
+                    error%found = .TRUE.
+                    error%msg = "I found a null or negative bin index in compute_rdf.f90.\\ STOP"
+                END IF
+                recurrence_bin(bin) =recurrence_bin(bin) +1
+                rdf           (bin) =rdf(bin) +array3D(i,j,k)
+            END DO
+
+            !normalize the rdf
+            WHERE (recurrence_bin/=0)
+                rdf = rdf/REAL(recurrence_bin,dp)
+            ELSEWHERE
+                rdf = 0.0_dp
+            END WHERE
+            
+            DEALLOCATE (recurrence_bin)
+        END SUBROUTINE
+        !===========================================================================================================================
+
+        !===========================================================================================================================
+        SUBROUTINE UTest_histogram_3D
+            REAL(dp), ALLOCATABLE :: nullarray3D (:,:,:) 
+            INTEGER(i2b) :: bin
+            ALLOCATE (nullarray3D (spaceGrid%n_nodes(1),spaceGrid%n_nodes(2),spaceGrid%n_nodes(3)) ,SOURCE=0._dp)
+
+            ! Test 1
+            nullarray3D = 0._dp
+            CALL histogram_3d (nullarray3D, rdf)
+            IF (ANY(rdf/=0._dp)) STOP "Test 1 in UTest_histogram_3D not passed."
+
+            ! Test 2
+            nullarray3D = 100._dp
+            CALL histogram_3d (nullarray3D, rdf)
+            IF (ANY(rdf<0._dp)) STOP "Test 2 in UTest_histogram_3D not passed."
+
+            ! Test 3
+            nullarray3D = -1._dp
+            CALL histogram_3d (nullarray3D, rdf)
+            IF (ANY(rdf>0._dp)) STOP "Test 3 in UTest_histogram_3D not passed."
+
+            ! Test 4
+            nullarray3D = 100._dp
+            CALL histogram_3d (nullarray3D, rdf)
+            IF (ANY(rdf>100._dp)) STOP "Test 4 in UTest_histogram_3D not passed."
+
+            DEALLOCATE (nullarray3D)
+        END SUBROUTINE UTest_histogram_3D
+        !===========================================================================================================================
 
 END SUBROUTINE compute_rdf
