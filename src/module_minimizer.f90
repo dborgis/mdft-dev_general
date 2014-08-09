4083cd9199f70eef8685deb0aaa59996540f3537
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-08-09 14:23:14 +0200

    Add subroutine that deals with cg_vect to extract rho and another that gets n from cgvect and a last one that deallocates solvent%rho

--- src/module_minimizer.f90
+++ src/module_minimizer.f90
@@ -43,5 +43,97 @@ module minimizer
             IF (ALLOCATED ( indicg ) ) deallocate ( indicg )
         END SUBROUTINE finalizeMinimizer
         !===========================================================================================================================
+        
+        !===========================================================================================================================
+        subroutine from_cgvect_get_rho
+        !===========================================================================================================================
+            use system      ,only: nb_species, spacegrid, solvent, nb_species
+            use quadrature  ,only: anggrid, molrotgrid
+            implicit none
+            integer(i2b) :: i, j, k, o, p, s, icg
+            integer(i2b) :: nfft(3)
 
+            nfft = spacegrid%n_nodes
+
+            do concurrent (s=1:nb_species)
+                if (.not. allocated (solvent(s)%rho)) then
+                    allocate (solvent(s)%rho(nfft(1),nfft(2),nfft(3),anggrid%n_angles,molrotgrid%n_angles), source=0._dp ,stat=i)
+                else
+                    i = 0
+                    solvent(s)%rho = 0._dp
+                end if
+            end do
+            if (i /= 0) stop "ERROR in allocate solvent%rho in energy_nn_cs.f90 > from_cgvect_get_rho"
+
+            icg = 0
+            do s =1, nb_species
+                do i =1, spacegrid%n_nodes(1)
+                    do j =1, spacegrid%n_nodes(2)
+                        do k =1, spacegrid%n_nodes(3)
+                            do o =1, anggrid%n_angles
+                                do p =1, molrotgrid%n_angles
+                                    icg = icg+1
+                                    solvent(s)%rho(i,j,k,o,p) = cg_vect(icg)**2 *solvent(s)%rho0
+                                end do
+                            end do
+                        end do
+                    end do
+                end do
+            end do
+        end subroutine from_cgvect_get_rho
+        !===========================================================================================================================
+        
+        !===========================================================================================================================
+        subroutine from_rho_get_n
+        !===========================================================================================================================
+            use system      ,only: nb_species, spacegrid, solvent, nb_species
+            use quadrature  ,only: anggrid, molrotgrid
+            implicit none
+            integer(i2b) :: i,j,k,o,p,s
+            integer(i2b) :: nfft(3)
+            
+            nfft = spacegrid%n_nodes
+
+            do concurrent (s=1:nb_species)
+                if (.not. allocated(solvent(s)%n)) then
+                    allocate (solvent(s)%n(nfft(1),nfft(2),nfft(3)), source=0._dp ,stat=i)
+                else
+                    i = 0
+                    solvent(s)%n = 0._dp
+                end if
+            end do
+            if (i /= 0) stop "ERROR in allocate solvent%n in energy_nn_cs.f90 > from_rho_get_n"
+
+            do s =1, nb_species
+                do i =1, spacegrid%n_nodes(1)
+                    do j =1, spacegrid%n_nodes(2)
+                        do k =1, spacegrid%n_nodes(3)
+                            do o =1, anggrid%n_angles
+                                do p =1, molrotgrid%n_angles
+                                    solvent(s)%n(i,j,k) = solvent(s)%n(i,j,k)   &
+                                        + solvent(s)%rho(i,j,k,o,p) * angGrid%weight(o) * molRotGrid%weight(p)
+                                end do
+                            end do
+                        end do
+                    end do
+                end do
+            end do
+            
+!~             do concurrent (s=1:nb_species, i=1:spacegrid%n_angles(1), j=1:spacegrid%n_angles(2), k=1:spacegrid%n_angles(3), &
+!~                            o=1:anggrid%n_angles, p=1:molrotgrid%n_angles)  ! TODO WHY DOESNT IT WORK??
+        end subroutine from_rho_get_n
+        !===========================================================================================================================
+        
+        
+        !===========================================================================================================================
+        subroutine deallocate_solvent_rho
+        !===========================================================================================================================
+            use system  ,only: solvent, nb_species
+            integer(i2b) :: s
+            do concurrent (s=1:nb_species)  ! solvent%rho is now useless
+                deallocate (solvent(s)%rho)
+            end do
+        end subroutine deallocate_solvent_rho
+        !===========================================================================================================================
+        
 end module minimizer
