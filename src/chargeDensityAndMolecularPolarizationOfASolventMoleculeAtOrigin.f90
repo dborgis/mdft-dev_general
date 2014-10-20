6ec153911dafa9005327da7dd500caf8eb7e711a
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-10-21 00:10:38 +0200

    solvent charge density sigma_k now series expanded to hexadecapole order

--- src/chargeDensityAndMolecularPolarizationOfASolventMoleculeAtOrigin.f90
+++ src/chargeDensityAndMolecularPolarizationOfASolventMoleculeAtOrigin.f90
@@ -3,82 +3,74 @@
 !                       -The solvent molecular polarization (from Ranieriet al : J. Chem. Phys. 98 (11) 1993) that can be used
 !into energy_polarization_..._.f90 to compute the (multipolar) polarization Free energy.
 
-SUBROUTINE chargeDensityAndMolecularPolarizationOfASolventMoleculeAtOrigin (Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotzz)
+subroutine chargeDensityAndMolecularPolarizationOfASolventMoleculeAtOrigin (Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotzz)
 
-    USE precision_kinds     ,ONLY: i2b, dp
-    USE constants           ,ONLY: iC=>i_complex, zeroC
-    USE system              ,ONLY: solvent, spaceGrid
-    USE quadrature          ,ONLY: angGrid, molRotGrid
-    USE fft                 ,ONLY: kx, ky, kz, k2
+  use precision_kinds     ,ONLY: i2b, dp
+  use constants           ,ONLY: iC=>i_complex, zeroC
+  use system              ,ONLY: solvent, spaceGrid
+  use quadrature          ,ONLY: angGrid, molRotGrid
+  use fft                 ,ONLY: kx, ky, kz, k2
+  use mathematica         ,ONLY: factorial
 
-    IMPLICIT NONE
+  implicit none
 
-    REAL(dp), DIMENSION(angGrid%n_angles,molRotGrid%n_angles), INTENT(IN) :: Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotzz
-    integer, pointer :: nfft1 => spacegrid%n_nodes(1), nfft2 => spacegrid%n_nodes(2), nfft3 => spacegrid%n_nodes(3)
-    INTEGER(i2b) :: i, j, k, o, p, n, s
-    REAL(dp)     :: r(3), Rc, kr, kvec(3), prefactor
-    COMPLEX(dp)  :: fac
+  real(dp), dimension(angGrid%n_angles,molRotGrid%n_angles), intent(in) :: Rotxx,Rotxy,Rotxz,Rotyx,Rotyy,Rotyz,Rotzx,Rotzy,Rotzz
+  integer, pointer :: nfft1 => spacegrid%n_nodes(1), nfft2 => spacegrid%n_nodes(2), nfft3 => spacegrid%n_nodes(3)
+  integer(i2b) :: i, j, k, o, p, n, s
+  real(dp)     :: r(3), kr, kvec(3)
+  complex(dp)  :: fac, X
+  type :: smoother_type
+    real(dp) :: radius = 0.5_dp ! dramaticaly important
+    real(dp) :: factor
+  end type smoother_type
+  type (smoother_type) :: smoother
 
-    !            ====================================================
-    !            !    	Initialization				!
-    !            !							!
-    !            ====================================================
-    Rc=0.0_dp
-    PRINT*,"WARNING YOU CHANGED SIGMA_K RADIUS FROM 0.5 TO 0"
-    IF (Rc/=0.0_dp) THEN
-        PRINT*,'WARNING: you convolute the solvent molecular charge density and the solvent molecular polarization &
-                &with a Gaussian of radius 0.5. Angstroms in chargeDensity...'
-    END IF
+  ! sigma_k is the Fourier transformed charge density of a single solvent molecule in the reference frame defined by solvent.in
+  ! molec_polar_k is the Fourier transformed molecular polarization
+  do concurrent( s=1:size(solvent) , sum(abs(solvent(s)%site%q))>epsilon(1._dp)) ! mask elimitates solvent molecules without point charges
+    allocate( solvent(s)%sigma_k(nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles), SOURCE=zeroC)
+    allocate( solvent(s)%molec_polar_k(3,nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles), SOURCE=zeroC)
+  end do
 
-    do concurrent( s=1:size(solvent) )
-        allocate( solvent(s)%sigma_k(nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles), SOURCE=zeroC)
-        allocate( solvent(s)%molec_polar_k(3,nfft1/2+1, nfft2, nfft3, angGrid%n_angles, molRotGrid%n_angles), SOURCE=zeroC)
+  do concurrent ( i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3, s=1:size(solvent)  , sum(abs(solvent(s)%site%q))>epsilon(1._dp)) ! mask elimitates solvent molecules without point charges)
+
+    kvec = [ kx(i), ky(j), kz(k) ]
+    smoother%factor =  exp(-smoother%radius**2 *k2(i,j,k)/2)
+
+    do concurrent ( o=1:angGrid%n_angles, p=1:molRotGrid%n_angles, &
+            n=1:SIZE(solvent(s)%site), (abs(solvent(s)%site(n)%q)>epsilon(1.0_dp)) )
+
+      r(1) = dot_product(   [Rotxx(o,p),Rotxy(o,p),Rotxz(o,p)]  ,  solvent(s)%site(n)%r  )
+      r(2) = dot_product(   [Rotyx(o,p),Rotyy(o,p),Rotyz(o,p)]  ,  solvent(s)%site(n)%r  )
+      r(3) = dot_product(   [Rotzx(o,p),Rotzy(o,p),Rotzz(o,p)]  ,  solvent(s)%site(n)%r  )
+      kr = dot_product( kvec, r )
+      X = -iC*kr
+      ! solvent(s)%sigma_k(i,j,k,o,p) = solvent(s)%sigma_k(i,j,k,o,p) + solvent(s)%site(n)%q *exp(X) *smoother%factor ! exact
+      solvent(s)%sigma_k(i,j,k,o,p) = solvent(s)%sigma_k(i,j,k,o,p) +solvent(s)%site(n)%q* sum([(X**i/factorial(i), i=0,4)])&
+            * smoother%factor ! Series expansion of exp(x) at 0 => multipole expansion of Vcoul(x). i=4 :: hexadecapole (16)
+      if ( abs(kr)<=epsilon(1.0_dp) ) then
+        solvent(s)%molec_polar_k(:,i,j,k,o,p) = solvent(s)%molec_polar_k(:,i,j,k,o,p) + solvent(s)%site(n)%q *r
+      else
+        fac = -iC*(exp(iC*kr)-1._dp)/kr *smoother%factor
+        solvent(s)%molec_polar_k(:,i,j,k,o,p) = solvent(s)%molec_polar_k(:,i,j,k,o,p) + fac*solvent(s)%site(n)%q *r
+      end if
+
     end do
+  end do
 
-    !            ====================================================
-    !            !    Compute sigma and molecular polarization   	!
-    !            !							                        !
-    !            ====================================================
+  call removeTraceOfMolecularPolarization
 
-    do concurrent ( i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3, s=1:size(solvent) )
+contains
 
-        kvec = [ kx(i), ky(j), kz(k) ]
-        prefactor =  exp(-Rc**2*k2(i,j,k)/2)
-
-        do concurrent ( o=1:angGrid%n_angles, p=1:molRotGrid%n_angles, &
-                        n=1:SIZE(solvent(s)%site), (abs(solvent(s)%site(n)%q)>=epsilon(1.0_dp)) )
-
-            r(1) = dot_product(   [Rotxx(o,p),Rotxy(o,p),Rotxz(o,p)]  ,  solvent(s)%site(n)%r  )
-            r(2) = dot_product(   [Rotyx(o,p),Rotyy(o,p),Rotyz(o,p)]  ,  solvent(s)%site(n)%r  )
-            r(3) = dot_product(   [Rotzx(o,p),Rotzy(o,p),Rotzz(o,p)]  ,  solvent(s)%site(n)%r  )
-
-            kr = dot_product( kvec, r )
-
-            solvent(s)%sigma_k(i,j,k,o,p) = solvent(s)%sigma_k(i,j,k,o,p) + solvent(s)%site(n)%q *EXP(-iC*kr) *prefactor
-
-            if ( abs(kr)<=epsilon(1.0_dp) ) THEN
-                solvent(s)%molec_polar_k(:,i,j,k,o,p) = solvent(s)%molec_polar_k(:,i,j,k,o,p) + solvent(s)%site(n)%q *r
-            else
-                fac = (EXP(iC*kr)-1)/kr*(-iC) *prefactor
-                solvent(s)%molec_polar_k(:,i,j,k,o,p) = solvent(s)%molec_polar_k(:,i,j,k,o,p) + fac*solvent(s)%site(n)%q *r
-            end if
-
-        end do
+  subroutine removeTraceOfMolecularPolarization
+    use precision_kinds, only: i2b
+    use quadrature, only: molRotGrid, angGrid
+    implicit none
+    integer(i2b) :: i,j,k,s,d
+    do concurrent (i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3, s=1:size(solvent), d=1:3)
+        solvent(s)%molec_polar_k(d,i,j,k,:,:) = solvent(s)%molec_polar_k(d,i,j,k,:,:)  &
+                      -sum( solvent(s)%molec_polar_k(d,i,j,k,:,:) ) /real(angGrid%n_angles*molRotGrid%n_angles,dp)
     end do
-
-    CALL normalizeMolecularDensity
-
-    CONTAINS
-
-        SUBROUTINE normalizeMolecularDensity
-            REAL(dp)          :: angle_number
-            INTEGER(i2b)      :: i,j,k,s,d
-            angle_number = angGrid%n_angles * molRotGrid%n_angles
-            IF (angle_number<1) STOP "I found no angle in normalizeMolecularDensity"
-            DO CONCURRENT (i=1:nfft1/2+1, j=1:nfft2, k=1:nfft3, s=1:size(solvent), d=1:3)
-                solvent(s)%molec_polar_k(d,i,j,k,:,:) = solvent(s)%molec_polar_k(d,i,j,k,:,:)  &
-                                                       -SUM( solvent(s)%molec_polar_k(d,i,j,k,:,:) ) /real(angle_number,dp)
-            END DO
-        END SUBROUTINE normalizeMolecularDensity
+  end subroutine removeTraceOfMolecularPolarization
 
 END SUBROUTINE chargeDensityAndMolecularPolarizationOfASolventMoleculeAtOrigin
