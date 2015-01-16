cad72beb0209d3d484af054c21267f3ba7b5e7c0
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-01-16 23:21:25 +0100

    cubic splines

--- src/module_mathematica.f90
+++ src/module_mathematica.f90
@@ -6,7 +6,8 @@ MODULE mathematica
     IMPLICIT NONE
     PRIVATE
     PUBLIC :: chop, TriLinearInterpolation, UTest_TrilinearInterpolation, floorNode, UTest_floorNode, ceilingNode, &
-        UTest_ceilingNode, distToFloorNode, UTest_distToFloorNode, factorial, deduce_optimal_histogram_properties
+        UTest_ceilingNode, distToFloorNode, UTest_distToFloorNode, factorial, deduce_optimal_histogram_properties, &
+        splint, spline
 
     CONTAINS
 
@@ -326,6 +327,81 @@ MODULE mathematica
     nbins    = ceiling( 2*real(n)**(1._dp/3._dp) ) ! Rice Rule, see http://en.wikipedia.org/wiki/Histogram
     binwidth = maxrange/real(nbins,dp) ! Width of each bin of the histogram
   end subroutine deduce_optimal_histogram_properties
+  !=================================================================================================================================
+
+  !=================================================================================================================================
+  subroutine spline(x,y,n,yp1,ypn,y2)
+  !=================================================================================================================================
+    implicit none
+    integer, intent(in) :: n
+    real(dp), intent(in) :: yp1,ypn,x(n),y(n)
+    real(dp), intent(out) :: y2(n)
+    ! Given arrays x(1:n) and y(1:n) containing a tabulated function, i.e., yi = f(xi), with
+    ! x1 < x2 < ... < xN , and given values yp1 and ypn for the first derivative of the interpolating function
+    ! at points 1 and n, respectively, this routine returns an array y2(1:n) of
+    ! length n which contains the second derivatives of the interpolating function at the tabulated
+    ! points xi. If yp1 and/or ypn are equal to 10^30 or larger, the routine is signaled to set
+    ! the corresponding boundary condition for a natural spline, with zero second derivative on
+    ! that boundary.
+    ! see http://www.haoli.org/nr/bookfpdf/f3-3.pdf
+    integer :: i,k
+    real(dp) :: p, qn, sig, un, u(n)
+    if(yp1>0.99e30) then
+      y2(1)=0._dp
+      u(1)=0._dp
+    else
+      y2(1)=-0.5
+      u(1)=(3./(x(2)-x(1)))*((y(2)-y(1))/(x(2)-x(1))-yp1)
+    end if
+    do i=2,n-1
+      sig=(x(i)-x(i-1))/(x(i+1)-x(i-1))
+      p=sig*y2(i-1)+2
+      y2(i)=(sig-1)/p
+      u(i)=(6.*((y(i+1)-y(i))/(x(i+1)-x(i))-(y(i)-y(i-1))/(x(i)-x(i-1)))/(x(i+1)-x(i-1))-sig*u(i-1))/p
+    end do
+    if (ypn>0.99e30) then
+      qn=0.
+      un=0.
+    else
+      qn=0.5
+      un=(3./(x(n)-x(n-1)))*(ypn-(y(n)-y(n-1))/(x(n)-x(n-1)))
+    end if
+    y2(n)=(un-qn*u(n-1))/(qn*y2(n-1)+1.)
+    do k=n-1,1,-1
+      y2(k)=y2(k)*y2(k+1)+u(k)
+    end do
+  end subroutine spline
+  !=================================================================================================================================
+
+  !=================================================================================================================================
+  subroutine splint(xa,ya,y2a,n,x,y)
+  !=================================================================================================================================
+    implicit none
+    INTEGER, intent(in) :: n
+    REAL(dp), intent(in) :: x,xa(n),y2a(n),ya(n)
+    real(dp), intent(out) :: y
+    !Given the arrays xa(1:n) and ya(1:n) of length n, which tabulate a function (with the
+    !  xai ’s in order), and given the array y2a(1:n), which is the output from spline above,
+    !  and given a value of x, this routine returns a cubic-spline interpolated value y
+    INTEGER :: k,khi,klo
+    REAL(dp) :: a,b,h
+    klo=1
+    khi=n
+    1 if (khi-klo>1) then
+      k=(khi+klo)/2
+      if(xa(k).gt.x)then
+        khi=k
+      else
+        klo=k
+      end if
+      goto 1
+    end if
+    h=xa(khi)-xa(klo)
+    if (h.eq.0.) stop "bad xa input in splint"
+    a=(xa(khi)-x)/h
+    b=(x-xa(klo))/h
+    y=a*ya(klo)+b*ya(khi)+ ((a**3-a)*y2a(klo)+(b**3-b)*y2a(khi))*(h**2)/6.
+  end subroutine splint
   !=================================================================================================================================
 
 END MODULE
