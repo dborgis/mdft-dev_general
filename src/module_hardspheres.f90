e03dda13f478f93e42129de47aea59f3d8b9043b
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-02-18 23:36:23 +0100

    :lipstick:+weight functions in k space stay real

--- src/module_hardspheres.f90
+++ src/module_hardspheres.f90
@@ -1,91 +1,79 @@
-MODULE hardspheres
+module hardspheres
 
-  USE precision_kinds  ,ONLY: dp
-  IMPLICIT NONE
+  use precision_kinds  ,only: dp
 
-  PRIVATE
+  implicit none
+  private
 
-  TYPE type_hs
-    REAL(dp) :: R          ! radius
-    REAL(dp) :: excchempot ! excess chemical potential
-    REAL(dp) :: Fexc0
-    REAL(dp) :: pf         ! packing fraction
-    COMPLEX(dp), ALLOCATABLE :: w_k(:,:,:,:)
-  END TYPE type_hs
-  TYPE(type_hs), PUBLIC, ALLOCATABLE :: hs(:) ! one per constituant
+  type type_hs
+    real(dp) :: r          ! radius
+    real(dp) :: excchempot ! excess chemical potential
+    real(dp) :: fexc0
+    real(dp) :: pf         ! packing fraction
+    real(dp), allocatable :: w_k(:,:,:,:) ! weight function in Fourier space (i,j,k,0:3)
+  end type type_hs
+  type(type_hs), public, allocatable :: hs(:) ! one per constituant
+  public :: packfrac, compute_hard_spheres_parameters
 
-  PUBLIC :: packfrac, populate_weight_functions_in_Fourier_space, compute_hard_spheres_parameters
+contains
 
+!===================================================================================================================================
+! packing fraction : eta = 4/3 * pi * R^3 * number density of the constituant ( /= total solvent density)
+pure function packfrac (numberdensity, HSradius)
+  use precision_kinds ,only: dp
+  use constants       ,only: pi
+  implicit none
+  real(dp) :: packfrac
+  real(dp), parameter  :: fourthird=4./3.
+  real(dp), intent(in) :: numberdensity ! density of the constituant (/= total solvant density)
+  real(dp), intent(in) :: HSradius ! radius of the constituant
+  packfrac = fourthird*pi*numberdensity*HSradius**3
+end function packfrac
 
-
-CONTAINS
-
-  !===================================================================================================================================
-  ! packing fraction : eta = 4/3 * pi * R^3 * number density of the constituant ( /= total solvent density)
-  pure function packfrac (numberdensity, HSradius)
-    use precision_kinds ,only: dp
-    use constants       ,only: pi
-    implicit none
-    real(dp) :: packfrac
-    real(dp), parameter  :: fourthird=4./3.
-    real(dp), intent(in) :: numberdensity ! density of the constituant (/= total solvant density)
-    real(dp), intent(in) :: HSradius ! radius of the constituant
-    packfrac = fourthird*pi*numberdensity*HSradius**3
-  end function packfrac
-  !===================================================================================================================================
-
-  !===========================================================================================================================
-  ! This SUBROUTINE computes the density independant weight functions as defined by Kierlik and Rosinberg in 1990
-  !===========================================================================================================================
-  ! The four weight functions are here defined in k-space. They are known analyticaly and only depend on the so called fundamental
-  ! measures of the hard spheres.
-  ! w_3i(k=0) = V_i the volume of constituant i
-  ! w_2i(k=0) = S_i the surface area of constituant i
-  ! w_1i(k=0) = R_i the radius of constituant i
-  ! w_0i(k=0) = 1
-  ! They are scalar numbers in opposition to scalar and vector weight functions by Rosenfeld in its seminal Phys. Rev. Lett.
-  ! introducing the fundamental measure theory (FMT)
-  !===========================================================================================================================
-  SUBROUTINE populate_weight_functions_in_Fourier_space
-
-    USE precision_kinds ,ONLY: dp, i2b
-    USE constants       ,ONLY: FourPi, zeroC
-    USE system          ,ONLY: nb_species, spaceGrid
-    USE fft             ,ONLY: norm_k
-    IMPLICIT NONE
-
-    REAL(dp) :: kR , FourPiR , sinkR , coskR, norm_k_local
-    INTEGER(i2b) :: l,m,n,s,nfft(3)
-
-    nfft = spaceGrid%n_nodes
-
-    DO CONCURRENT (s=1:nb_species)
-      ALLOCATE( hs(s)%w_k(nfft(1)/2+1, nfft(2), nfft(3), 0:3 ) ,SOURCE=zeroC)
-    END DO
-
-    ! Weight functions of a fluid of hard spheres are known analyticaly. They are easily defined in Fourier space.
-    ! They depend on fundamental measures of the fluid.
-    ! Here is the Kierlik and Rosinberg'FMT : 4 scalar weight functions by species. Ref: Kierlik and Rosinberg, PRA 1990
-
-    DO CONCURRENT ( s=1:nb_species, l=1:nfft(1)/2+1, m=1:nfft(2), n=1:nfft(3) )
-      FourPiR = FourPi * hs(s)%R
-      IF ( l+m+n/=3 ) THEN !  => norm_k_local /= 0.0_dp
-        norm_k_local = norm_k (l,m,n)
-        kR = norm_k_local * hs(s)%R
-        sinkR = SIN(kR)
-        coskR = COS(kR)
-        hs(s)%w_k(l,m,n,3) = CMPLX(FourPi * ( sinkR - kR * coskR ) / ( norm_k_local ** 3 ) ,0._dp, dp)
-        hs(s)%w_k(l,m,n,2) = CMPLX(FourPiR * sinkR / norm_k_local ,0._dp, dp)
-        hs(s)%w_k(l,m,n,1) = CMPLX(( sinkR + kR * coskR ) / ( 2.0_dp * norm_k_local ) ,0._dp, dp)
-        hs(s)%w_k(l,m,n,0) = CMPLX(coskR + 0.5_dp * kR * sinkR ,0._dp, dp)
-      ELSE
-        hs(s)%w_k(l,m,n,3) = CMPLX(FourPi / 3.0_dp * hs(s)%R** 3  ,0._dp, dp)! volume
-        hs(s)%w_k(l,m,n,2) = CMPLX(FourPi * hs(s)%R** 2  ,0._dp, dp)! surface area
-        hs(s)%w_k(l,m,n,1) = CMPLX(hs(s)%R  ,0._dp, dp)! radius
-        hs(s)%w_k(l,m,n,0) = CMPLX(1.0_dp  ,0._dp, dp)! unity
-      END IF
-    END DO
-  END SUBROUTINE populate_weight_functions_in_Fourier_space
+!===================================================================================================================================
+!Compute the weight functions defined by Kierlik and Rosinberg, Phys. Rev. A 42, 3382-3387 (1990)
+! The four weight functions are here defined in k-space. They are known analyticaly and only depend on the so called fundamental
+! measures of the hard spheres.
+! w_3i(k=0) = V_i the volume of constituant i
+! w_2i(k=0) = S_i the surface area of constituant i
+! w_1i(k=0) = R_i the radius of constituant i
+! w_0i(k=0) = 1
+! They are scalar numbers in opposition to scalar and vector weight functions by Rosenfeld in its seminal Phys. Rev. Lett.
+! introducing the fundamental measure theory (FMT)
+subroutine weight_functions
+  use precision_kinds ,ONLY: dp, i2b
+  use constants       ,ONLY: fourpi, zeroC, zerodp
+  use system          ,ONLY: nb_species, spaceGrid
+  use fft             ,ONLY: norm_k
+  implicit none
+  real(dp)     :: kR, sinkR, coskR, norm_k_loc, R, k
+  integer(i2b) :: l,m,n,s,nfft1,nfft2,nfft3
+  nfft1=spacegrid%n_nodes(1)
+  nfft2=spacegrid%n_nodes(2)
+  nfft3=spacegrid%n_nodes(3)
+  do concurrent( s=1:nb_species )
+    allocate( hs(s)%w_k(nfft1/2+1,nfft2,nfft3,0:3) ,source=zerodp)
+  end do
+  ! Weight functions of a fluid of hard spheres are known analyticaly.
+  do concurrent( s=1:nb_species, l=1:nfft1/2+1, m=1:nfft2, n=1:nfft3 )
+    R=hs(s)%R
+    k=norm_k(l,m,n)
+    if( abs(k)>epsilon(1._dp) ) then !  => k /= 0.0_dp
+      kR=k*R
+      sinkR=sin(kR)
+      coskR=cos(kR)
+      hs(s)%w_k(l,m,n,3) = fourpi*(sinkR-kR*coskR)/(k**3)
+      hs(s)%w_k(l,m,n,2) = fourpi*R*sinkR/k
+      hs(s)%w_k(l,m,n,1) = (sinkR+kR*coskR)/(2*k)
+      hs(s)%w_k(l,m,n,0) = coskR+kR*sinkR/2
+    else
+      hs(s)%w_k(l,m,n,3) = fourpi/3*R**3 ! volume
+      hs(s)%w_k(l,m,n,2) = fourpi  *R**2 ! surface area
+      hs(s)%w_k(l,m,n,1) = R             ! radius
+      hs(s)%w_k(l,m,n,0) = 1             ! unity
+    end if
+  end do
+end subroutine weight_functions
 
   !===================================================================================================================================
   ! Read, allocate, compute the hard sphere parameters
@@ -99,16 +87,16 @@ CONTAINS
   !===================================================================================================================================
   SUBROUTINE compute_hard_spheres_parameters
 
-    USE system          ,ONLY: nb_species, solvent
-    USE input           ,ONLY: input_char
+    use system          ,ONLY: nb_species, solvent
+    use input           ,ONLY: input_char
 
-    IMPLICIT NONE
+    implicit none
 
     CHARACTER(4) :: hs_functional
 
     hs_functional = input_char('hs_functional')
     CALL read_hard_sphere_radius_and_allocate_if_necessary ! read hard sphere radius and allocate if necessary
-    CALL populate_weight_functions_in_Fourier_space
+    CALL weight_functions
     CALL compute_packing_fractions_and_check_legality
     CALL check_functional_legality ( hs_functional ) ! Get the free energy functional that should be used. For now Percus Yevick and Carnahan Starling only. May be expanded.
 
@@ -124,11 +112,11 @@ CONTAINS
     ! they would be unphysical. 0.74 is the maximum packing of a solid crystal.
     !===============================================================================================================================
     SUBROUTINE compute_packing_fractions_and_check_legality
-      USE precision_kinds ,ONLY: dp , i2b
-      USE system          ,ONLY: nb_species
-      USE input           ,ONLY: verbose
-      IMPLICIT NONE
-      INTEGER(i2b) :: s
+      use precision_kinds ,ONLY: dp , i2b
+      use system          ,ONLY: nb_species
+      use input           ,ONLY: verbose
+      implicit none
+      integer(i2b) :: s
       DO s = 1, nb_species
         hs(s)%pf = packfrac( numberdensity=solvent(s)%n0, HSradius=hs(s)%R )
         IF (verbose) PRINT*,'Packing fraction of species ',s,') is ',hs(s)%pf
@@ -148,11 +136,11 @@ CONTAINS
     ! this SUBROUTINE checks if the functional asked in input file is legal. Else, stop execution.
     !===============================================================================================================================
     SUBROUTINE check_functional_legality ( hs_functional )
-      USE precision_kinds  ,ONLY: i2b
-      USE system           ,ONLY: nb_species
-      IMPLICIT NONE
+      use precision_kinds  ,ONLY: i2b
+      use system           ,ONLY: nb_species
+      implicit none
       CHARACTER(4), INTENT(INOUT) :: hs_functional
-      INTEGER(i2b):: i
+      integer(i2b):: i
 
       i = 0
       IF ( hs_functional(1:2) == 'CS' ) i = i+1
@@ -178,18 +166,18 @@ CONTAINS
     SUBROUTINE excess_chemical_potential_and_reference_bulk_grand_potential &
       ( hs_functional )
 
-      USE precision_kinds ,ONLY: dp, i2b
-      USE constants       ,ONLY: fourpi, pi
-      USE system          ,ONLY: thermoCond, spaceGrid
-      USE input           ,ONLY: verbose
+      use precision_kinds ,ONLY: dp, i2b
+      use constants       ,ONLY: fourpi, pi
+      use system          ,ONLY: thermoCond, spaceGrid
+      use input           ,ONLY: verbose
 
-      IMPLICIT NONE
+      implicit none
 
       CHARACTER(4), INTENT(IN) :: hs_functional
-      REAL(dp) :: n0, n1, n2, n3 ! weighted densities in the case of constant density = ref bulk density
-      REAL(dp) :: dphidn(0:3) ! partial derivative of phi w.r.t. weighted densities
-      REAL(dp) :: dndrho(0:3) ! partial derivative of weighted densities w.r.t. density of constituant i
-      INTEGER(i2b) :: s ! dummy
+      real(dp) :: n0, n1, n2, n3 ! weighted densities in the case of constant density = ref bulk density
+      real(dp) :: dphidn(0:3) ! partial derivative of phi w.r.t. weighted densities
+      real(dp) :: dndrho(0:3) ! partial derivative of weighted densities w.r.t. density of constituant i
+      integer(i2b) :: s ! dummy
 
       DO s=1,nb_species ! compute excess chemical potential, so that bulk grand potential is zero for density = constant = ref bulk density
 
