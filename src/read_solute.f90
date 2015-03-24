396549c8024b058ac847446e0f64b6d12ad8110f
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-03-24 15:49:23 +0100

    compute spacegrid length with a buffer That will be the default value; One should not rely on abritrary user wishes on Lx, Ly, lz

--- src/read_solute.f90
+++ src/read_solute.f90
@@ -1,75 +1,54 @@
 !> read solute atomic positions, charge, and lennard jones values in solute.in
 !! charge in electron units, sigma in Angstroms, epsilon in KJ/mol.
-SUBROUTINE read_solute
+subroutine read_solute
 
-    USE precision_kinds, ONLY: i2b,dp
-    USE system,          ONLY: nb_solute_sites, solute, nb_species, spaceGrid
-    USE input,           ONLY: input_line, input_log, input_dp
-    USE periodic_table,  ONLY: init_periodic_table, ptable
+  use precision_kinds, ONLY: i2b,dp
+  use system,          ONLY: nb_solute_sites, solute, nb_species, spaceGrid
+  use input,           ONLY: input_line, input_log, input_dp
+  use periodic_table,  ONLY: init_periodic_table, ptable
 
-    IMPLICIT NONE
+  implicit none
 
-    INTEGER(i2b) :: n,i,stat
-    REAL(dp) :: lx, ly, lz
+  integer :: n,i,stat
 
-    lx= spaceGrid%length(1)
-    ly= spacegrid%length(2)
-    lz= spaceGrid%length(3)
+  call init_periodic_table
+  ! print *, ptable ( 1 ) % name
+  ! open and test if input/solute.in is ok
 
-    CALL init_periodic_table
-    ! print *, ptable ( 1 ) % name
-    ! open and test if input/solute.in is ok
+  open (5, FILE='input/solute.in', STATUS='old', IOSTAT=stat)
+  IF (stat /= 0) THEN
+    PRINT*,'solute.in cannot be opened ! => STOP !'
+    STOP
+  END IF
 
-    OPEN (5, FILE='input/solute.in', STATUS='old', IOSTAT=stat)
-        IF (stat /= 0) THEN
-            PRINT*,'solute.in cannot be opened ! => STOP !'
-            STOP
-        END IF
+  READ (5,*) ! comment line
+  READ (5,*) nb_solute_sites ! total number of atom sites of the solute
+  ALLOCATE(solute%site(nb_solute_sites))
+  READ (5,*)
+  DO n = 1, nb_solute_sites
+    READ(5,*) i, solute%site(n)%q, solute%site(n)%sig, solute%site(n)%eps, &
+              solute%site(n)%lambda1, solute%site(n)%lambda2, solute%site(n)%r, solute%site(n)%Z
+    END DO
+  CLOSE (5)
+  solute%site%q = solute%site%q * input_dp('solute_charges_scale_factor', defaultvalue=1._dp)
 
-        READ (5,*) ! comment line
-        READ (5,*) nb_solute_sites ! total number of atom sites of the solute
-        ALLOCATE(solute%site(nb_solute_sites))
+  block
+    real(dp) :: solutexmin, solutexmax, soluteymin, soluteymax, solutezmin, solutezmax, &
+                solutediameterx, solutediametery, solutediameterz, solutesigmaljmax
+    solutexmin = minval(solute%site%r(1)) ; print*,"solutexmin=",solutexmin
+    soluteymin = minval(solute%site%r(2)) ; print*,"soluteymin=",soluteymin
+    solutezmin = minval(solute%site%r(3)) ; print*,"solutezmin=",solutezmin
+    solutexmax = maxval(solute%site%r(1)) ; print*,"solutexmin=",solutexmax
+    soluteymax = maxval(solute%site%r(2)) ; print*,"soluteymin=",soluteymax
+    solutezmax = maxval(solute%site%r(3)) ; print*,"solutezmin=",solutezmax
+    solutediameterx = sqrt((solutexmax-solutexmin)**2)
+    solutediametery = sqrt((soluteymax-soluteymin)**2)
+    solutediameterz = sqrt((solutezmax-solutezmin)**2)
+    solutesigmaljmax = maxval(solute%site%sig)
+    spacegrid%buffer_length = input_dp("buffer_length", defaultvalue=15._dp)
+    spacegrid%length(1) = solutediameterx +2*solutesigmaljmax +2.*spacegrid%buffer_length
+    spacegrid%length(2) = solutediametery +2*solutesigmaljmax +2.*spacegrid%buffer_length
+    spacegrid%length(3) = solutediameterz +2*solutesigmaljmax +2.*spacegrid%buffer_length
+  end block
 
-        READ (5,*)
-        DO n = 1, nb_solute_sites
-            READ(5,*) i, solute%site(n)%q, solute%site(n)%sig, solute%site(n)%eps, &
-                    solute%site(n)%lambda1, solute%site(n)%lambda2, solute%site(n)%r, solute%site(n)%Z
-        END DO
-    CLOSE (5)
-    solute%site%q = solute%site%q * input_dp('solute_charges_scale_factor', defaultvalue=1._dp)
-
-    CALL translate_to_center_of_supercell_if_needed ! if user wants all the sites to be translated to the center of the box, ie by Lx/2, Ly/2, Lz/2
-    CALL assert_inside ! check if cartesian coordinates read in input/solute.in are in the supercell
-    CALL print_supercell_xsf ! Print periodic XSF file to be read by VMD or equivalent
-
-
-    CONTAINS
-
-    ! if user asks for it (tag 'translate_solute_to_center'), add Lx/2, Ly/2, Lz/2 to all solute coordinates
-    SUBROUTINE translate_to_center_of_supercell_if_needed
-        USE input, ONLY: input_log
-        USE system, ONLY: spaceGrid
-        IF (input_log( 'translate_solute_to_center', defaultvalue=.true. )) THEN
-            solute%site%r(1) = solute%site%r(1) + spaceGrid%length(1)/2.0_dp
-            solute%site%r(2) = solute%site%r(2) + spaceGrid%length(2)/2.0_dp
-            solute%site%r(3) = solute%site%r(3) + spaceGrid%length(3)/2.0_dp
-        END IF
-    END SUBROUTINE translate_to_center_of_supercell_if_needed
-
-
-
-    SUBROUTINE assert_inside
-        USE SYSTEM, ONLY: spaceGrid
-        INTEGER(i2b) :: i
-        ! check if some positions are out of the supercell
-        !j is a test tag. We loop over this test until every atom is in the box.
-        ! This allows for instance, if a site is two boxes too far to still be ok.
-        DO CONCURRENT ( i=1:nb_solute_sites) 
-            solute%site(i)%r(1) = MODULO ( solute%site(i)%r(1) , spaceGrid%length(1) )
-            solute%site(i)%r(2) = MODULO ( solute%site(i)%r(2) , spaceGrid%length(2) )
-            solute%site(i)%r(3) = MODULO ( solute%site(i)%r(3) , spaceGrid%length(3) )
-        END DO
-    END SUBROUTINE assert_inside
-
-
-END SUBROUTINE read_solute
+end subroutine read_solute
