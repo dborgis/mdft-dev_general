e4c3e59d28cd3a078a3cca0b36a5c678c8bf2463
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-04-28 12:20:07 +0200

    use iso_c_binding

--- src/module_precision_kinds.f90
+++ src/module_precision_kinds.f90
@@ -1,15 +1,13 @@
 !> double is the module defining precision variables.
 !! This allows architecture independant programming.
-MODULE precision_kinds
-
-    IMPLICIT NONE
-        
-    INTEGER( KIND=KIND(1) ), PARAMETER :: i2b = KIND(1) ! usual integer 
-    INTEGER(i2b), PARAMETER :: dp = KIND(0.0d0)         ! usual double precision float
-    INTEGER(i2b), PARAMETER :: sp = KIND(0.0)           ! usual simple precision float
-    INTEGER(i2b), PARAMETER :: i4b = 2_i2b * i2b        ! usual double precision integer
-
-END MODULE precision_kinds
+module precision_kinds
+  use iso_c_binding, only: C_DOUBLE, C_FLOAT
+  implicit none
+  integer( KIND=KIND(1) ), parameter :: i2b = KIND(1) ! usual integer 
+  integer(i2b), parameter :: dp = C_DOUBLE         ! usual double precision float
+  integer(i2b), parameter :: sp = C_FLOAT           ! usual simple precision float
+  integer(i2b), parameter :: i4b = 2_i2b * i2b        ! usual double precision integer
+end module precision_kinds
 ! defining the precision kind this way we say to the computer : use the default single precision and double precision.
 ! remember that this default value may change depending on the architecture of your CPU.
 ! but one might one day wish to change that. It would be such a mess do change every number in the program !
