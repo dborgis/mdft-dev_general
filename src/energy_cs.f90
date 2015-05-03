f2de2cfcbc4f2b1fbd5d397fb7a10074193ba28a
Maximilien Levesque <maximilien.levesque@gmail.com>
2015-05-03 22:43:16 +0200

    use that cg_vect_new is now multidim

--- src/energy_cs.f90
+++ src/energy_cs.f90
@@ -19,6 +19,7 @@ subroutine energy_cs (cs, Fexcnn, dFexcnn, exitstatus)
   integer, intent(out) :: exitstatus
   type(cfile), intent(in) :: cs
   real(dp), intent(out) :: dFexcnn(:,:,:,:,:,:)
+  real(dp) :: n(spacegrid%n_nodes(1),spacegrid%n_nodes(2),spacegrid%n_nodes(3)), n_loc
 
   exitstatus=1 ! everything is fine
   nfft1 = spacegrid%n_nodes(1)
@@ -30,16 +31,22 @@ subroutine energy_cs (cs, Fexcnn, dFexcnn, exitstatus)
   kT = thermocond%kbT
   dV = spacegrid%dV
 
-  call from_cgvect_get_rho ! returns
-  call from_rho_get_n      ! returns solvent(:)%n(nfft1,nfft2,nfft3)
-  call deallocate_solvent_rho
   if( size(solvent)/=1 ) then
     exitstatus=-1
+    return
   else
     s=1
   end if
 
-  fftw3%in_forward = solvent(s)%n - solvent(s)%n0
+  do concurrent( i=1:nfft1, j=1:nfft2, k=1:nfft3 )
+    n_loc = 0
+    do concurrent( o=1:anggrid%n_angles, p=1:molrotgrid%n_angles )
+      n_loc = n_loc + cg_vect_new(i,j,k,o,p,s)**2*solvent(s)%rho0*molrotgrid%weight(p)*anggrid%weight(o)
+    end do
+    n(i,j,k) = n_loc
+  end do
+
+  fftw3%in_forward = n - solvent(s)%n0
   call dfftw_execute( fftw3%plan_forward )
 
   k2max=maxval(cs%x)
@@ -61,28 +68,14 @@ subroutine energy_cs (cs, Fexcnn, dFexcnn, exitstatus)
   ! at this point we have gamma(k)
 
   call dfftw_execute( fftw3%plan_backward )
-  fftw3%out_backward = fftw3%out_backward /(nfft1*nfft2*nfft3)
+  fftw3%out_backward = fftw3%out_backward /(nfft1*nfft2*nfft3) ! gamma(x)
 
   ! excess free energy
-  if( nb_species /= 1 ) error stop "Only nb_species=1 implemented in energy_cs.f90"
-  Fexcnn = -kT/2 *sum( (solvent(1)%n-solvent(1)%n0 )*fftw3%out_backward )*dV
+  Fexcnn = -kT/2 *sum( (n-solvent(1)%n0 )*fftw3%out_backward )*dV
 
   ! gradient
-  do s=1,nb_species
-    fact= -kT*dV*solvent(s)%rho0
-    do i=1,nfft1
-      do j=1,nfft2
-        do k=1,nfft3
-          Vint=fftw3%out_backward(i,j,k) *fact
-          do o=1,anggrid%n_angles
-            do p=1,molrotgrid%n_angles
-              psi=cg_vect_new(i,j,k,o,p,s)
-              dFexcnn(i,j,k,o,p,s) = 2*psi*anggrid%weight(o)*molrotgrid%weight(p)*Vint
-            end do
-          end do
-        end do
-      end do
-    end do
+  do concurrent( i=1:nfft1, j=1:nfft2, k=1:nfft3, o=1:anggrid%n_angles, p=1:molrotgrid%n_angles, s=1:nb_species )
+    dfexcnn(i,j,k,o,p,s) = 2 *cg_vect_new(i,j,k,o,p,s) *anggrid%weight(o) *molrotgrid%weight(p) *fftw3%out_backward(i,j,k) *(-kT*dV*solvent(s)%rho0)
   end do
 
 end subroutine energy_cs
