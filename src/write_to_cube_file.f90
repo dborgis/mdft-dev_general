4896c9f27a426ec39b349d492b3ca31aa9706a9d
Maximilien Levesque <maximilien.levesque@gmail.com>
2016-05-17 17:20:45 +0200

    update var names: n_nodes(1:3) => nx, ny, nz +:lipstick:

--- src/write_to_cube_file.f90
+++ src/write_to_cube_file.f90
@@ -8,43 +8,45 @@
 ! output : a file called 'filename' containing 3dimensional data in .cube format
 ! written by Maximilien Levesque, while in post doc at Ecole Normale Superieure, Paris in Daniel Borgis's theoretical chemistry group
 ! 20110919  Maximilien Levesque, clean version for Virginie M.
-SUBROUTINE write_to_cube_file ( array , filename )
+
+subroutine write_to_cube_file ( array , filename )
 
     use module_solute, only: solute
     use module_grid, only: grid
-    use precision_kinds, ONLY: i2b, dp
+    use precision_kinds, only: dp
 
-    IMPLICIT NONE
+    implicit none
 
-    INTEGER(i2b) :: i , j , k , n ! dummy variables
-    CHARACTER(50), INTENT(IN) :: filename ! filename of .cube file. For example : "density.cube"
-    REAL(dp), INTENT(IN), DIMENSION(grid%n_nodes(1), grid%n_nodes(2),grid%n_nodes(3)) :: array
-    REAL(dp), PARAMETER :: angtobohr = 1.889725989_dp ! 1 Bohr = 1.889725989 Ang. Necessary because of VMD understanding of lengths
+    integer :: i, j, k, n ! dummy variables
+    character(50), intent(in) :: filename ! filename of .cube file. For example : "density.cube"
+    real(dp), intent(in), dimension(grid%nx, grid%ny, grid%nz) :: array
+    real(dp), parameter :: angtobohr = 1.889725989_dp ! 1 Bohr = 1.889725989 Ang. Necessary because of VMD understanding of lengths
 
     ! define formats for writing to file
 
     ! first open the file you want to print array in. It's a formatted file.
-    open ( 10 , file = filename , form = 'formatted' )
-        write ( 10 , * ) ' CPMD CUBE FILE.' ! default text
-        write ( 10 , * ) ' OUTER LOOP: X, MIDDLE LOOP: Y, INNER LOOP: Z' ! default text, for remembering
+    open( 10 , file = filename , form = 'formatted' )
+        write(10,*) ' CPMD CUBE FILE.' ! default text
+        write(10,*) ' OUTER LOOP: X, MIDDLE LOOP: Y, INNER LOOP: Z' ! default text, for remembering
         ! write the total number of sites and a default text nobody knows it meaning
-        write ( 10 , * ) SIZE(solute%site) ,' 0.0 0.0 0.0 '
+        write(10,*) size(solute%site) ,' 0.0 0.0 0.0 '
         ! write primary vectors
-        write ( 10 , * ) grid%n_nodes(1),grid%dl(1)*angtobohr,' 0.0 0.0'
-        write ( 10 , * ) grid%n_nodes(2),' 0.0 ',grid%dl(2)*angtobohr, ' 0.0'
-        write ( 10 , * ) grid%n_nodes(3),' 0.0 0.0 ',grid%dl(3)*angtobohr
+        write(10,*) grid%nx, grid%dx*angtobohr,' 0.0 0.0'
+        write(10,*) grid%ny,' 0.0 ', grid%dy*angtobohr, ' 0.0'
+        write(10,*) grid%nz,' 0.0 0.0 ', grid%dz*angtobohr
         ! write the atoms and their coordinates in Bohr
-        DO n = 1, SIZE(solute%site)! , dim = 1 )
-            WRITE(10,*) solute%site(n)%Z, ' 0.0 ' , &
-                    solute%site(n)%r(1)*angtobohr, solute%site(n)%r(2)*angtobohr, solute%site(n)%r(3)*angtobohr
-        END DO
+        do n = 1, size(solute%site)! , dim = 1 )
+            write(10,*) solute%site(n)%Z, ' 0.0 ' , &
+              solute%site(n)%r(1)*angtobohr, solute%site(n)%r(2)*angtobohr, solute%site(n)%r(3)*angtobohr
+        end do
         ! write .cube file. One value per line. As said earlier, run over x then y then z. The one varying the most rapidly is z.
-        do i = 1 , grid%n_nodes(1)
-            do j = 1 , grid%n_nodes(2)
-                do k = 1 , grid%n_nodes(3)
-                    WRITE(10,*) real(array(i,j,k))
+        do i=1, grid%nx
+            do j=1, grid%ny
+                do k=1, grid%nz
+                    write(10,*) real(array(i,j,k))
                 end do
             end do
         end do
     close(10)
+
 end subroutine
