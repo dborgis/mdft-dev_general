100feb9eb8242d4acb19ef56c6f2484a0635693e
Maximilien Levesque <maximilien.levesque@gmail.com>
2016-05-18 11:12:06 +0200

    :lipstick: + do N continue N => do end do

--- src/module_blas_etc.f90
+++ src/module_blas_etc.f90
@@ -1,6 +1,8 @@
 module module_blas_etc
+
   use precision_kinds, only: dp
   implicit none
+
 contains
 
   pure subroutine daxpy(n,da,dx,incx,dy,incy)
@@ -63,52 +65,49 @@ contains
     return
   end function ddot
 
+  subroutine  dscal(n,da,dx,incx)
+    implicit none
+    real(dp) da,dx(*)
+    integer i,incx,m,mp1,n,nincx
+    if( n.le.0 .or. incx.le.0 )return
+    if(incx.eq.1)go to 20
+    nincx = n*incx
+    do 10 i = 1,nincx,incx
+      dx(i) = da*dx(i)
+    10 continue
+      return
+    20 m = mod(n,5)
+    if( m .eq. 0 ) go to 40
+    do 30 i = 1,m
+      dx(i) = da*dx(i)
+    30 continue
+    if( n .lt. 5 ) return
+    40 mp1 = m + 1
+    do 50 i = mp1,n,5
+      dx(i) = da*dx(i)
+      dx(i + 1) = da*dx(i + 1)
+      dx(i + 2) = da*dx(i + 2)
+      dx(i + 3) = da*dx(i + 3)
+      dx(i + 4) = da*dx(i + 4)
+    50 continue
+    return
+  end subroutine  dscal
 
-        subroutine  dscal(n,da,dx,incx)
-          implicit none
-        real(dp) da,dx(*)
-        integer i,incx,m,mp1,n,nincx
-        if( n.le.0 .or. incx.le.0 )return
-        if(incx.eq.1)go to 20
-        nincx = n*incx
-        do 10 i = 1,nincx,incx
-          dx(i) = da*dx(i)
-     10 continue
-        return
-     20 m = mod(n,5)
-        if( m .eq. 0 ) go to 40
-        do 30 i = 1,m
-          dx(i) = da*dx(i)
-     30 continue
-        if( n .lt. 5 ) return
-     40 mp1 = m + 1
-        do 50 i = mp1,n,5
-          dx(i) = da*dx(i)
-          dx(i + 1) = da*dx(i + 1)
-          dx(i + 2) = da*dx(i + 2)
-          dx(i + 3) = da*dx(i + 3)
-          dx(i + 4) = da*dx(i + 4)
-     50 continue
-        return
-        end subroutine  dscal
-
-
-subroutine dpofa(a,lda,n,info)
-          implicit none
-        integer lda,n,info
-        real(dp) a(lda,*)
-
-        real(dp) t!,ddot
-        real(dp) s
-        integer j,jm1,k
-           do 30 j = 1, n
-              info = j
-              s = 0.0d0
-              jm1 = j - 1
-              if (jm1 .lt. 1) go to 20
-              do k = 1, jm1
-                 t = a(k,j) - ddot(k-1,a(1,k),1,a(1,j),1)
-                 t = t/a(k,k)
+  subroutine dpofa(a,lda,n,info)
+    implicit none
+    integer lda,n,info
+    real(dp) a(lda,*)
+    real(dp) t!,ddot
+    real(dp) s
+    integer j,jm1,k
+    do 30 j = 1, n
+      info = j
+      s = 0.0d0
+      jm1 = j - 1
+      if (jm1 .lt. 1) go to 20
+      do k = 1, jm1
+        t = a(k,j) - ddot(k-1,a(1,k),1,a(1,j),1)
+        t = t/a(k,k)
                  a(k,j) = t
                  s = s + t*t
               end do
@@ -118,69 +117,68 @@ subroutine dpofa(a,lda,n,info)
               a(j,j) = sqrt(s)
      30    continue
            info = 0
-end subroutine dpofa
+  end subroutine dpofa
 
-!
-! linpack's dtrsl is the time-consuming routine of lbfgs
-!
-pure subroutine dtrsl(t,ldt,n,b,job,info)
-        implicit none
-        integer, intent(in) :: ldt,n,job
-        integer, intent(out) :: info
-        real(dp), intent(in) :: t(ldt,n)
-        real(dp), intent(inout) :: b(n)
-        real(dp) temp!,ddot
-        integer case,j,jj
-        do info = 1, n
-          if (t(info,info) .eq. 0.0d0) return
-        end do
-        info = 0
-        case = 1
-        if (mod(job,10) .ne. 0) case = 2
-           if (mod(job,100)/10 .ne. 0) case = case + 2
-           go to (20,50,80,110), case
-     20    continue
-              b(1) = b(1)/t(1,1)
-              if (n .lt. 2) go to 40
-              do j = 2, n
-                 temp = -b(j-1)
-                 call daxpy(n-j+1,temp,t(j,j-1),1,b(j),1)
-                 b(j) = b(j)/t(j,j)
-              end do
-     40       continue
-           go to 140
-     50    continue
-              b(n) = b(n)/t(n,n)
-              if (n .lt. 2) go to 70
-              do 60 jj = 2, n
-                 j = n - jj + 1
-                 temp = -b(j+1)
-                 call daxpy(j,temp,t(1,j+1),1,b(1),1)
-                 b(j) = b(j)/t(j,j)
-     60       continue
-     70       continue
-           go to 140
-     80    continue
-              b(n) = b(n)/t(n,n)
-              if (n .lt. 2) go to 100
-              do 90 jj = 2, n
-                 j = n - jj + 1
-                 b(j) = b(j) - ddot(jj-1,t(j+1,j),1,b(j+1),1)
-                 b(j) = b(j)/t(j,j)
-     90       continue
+  pure subroutine dtrsl(t,ldt,n,b,job,info)
+    !
+    ! linpack's dtrsl is the time-consuming routine of lbfgs
+    !
+    implicit none
+    integer, intent(in) :: ldt,n,job
+    integer, intent(out) :: info
+    real(dp), intent(in) :: t(ldt,n)
+    real(dp), intent(inout) :: b(n)
+    real(dp) temp!,ddot
+    integer case,j,jj
+    do info = 1, n
+      if (t(info,info) .eq. 0.0d0) return
+    end do
+    info = 0
+    case = 1
+    if (mod(job,10) .ne. 0) case = 2
+    if (mod(job,100)/10 .ne. 0) case = case + 2
+    go to (20,50,80,110), case
+    20  continue
+    b(1) = b(1)/t(1,1)
+    if (n .lt. 2) go to 40
+    do j = 2, n
+      temp = -b(j-1)
+      call daxpy(n-j+1,temp,t(j,j-1),1,b(j),1)
+      b(j) = b(j)/t(j,j)
+    end do
+    40       continue
+    go to 140
+    50    continue
+    b(n) = b(n)/t(n,n)
+    if (n .lt. 2) go to 70
+    do jj = 2, n
+      j = n - jj + 1
+      temp = -b(j+1)
+      call daxpy(j,temp,t(1,j+1),1,b(1),1)
+      b(j) = b(j)/t(j,j)
+    end do
+    70       continue
+    go to 140
+    80    continue
+    b(n) = b(n)/t(n,n)
+    if (n .lt. 2) go to 100
+    do jj = 2, n
+      j = n - jj + 1
+      b(j) = b(j) - ddot(jj-1,t(j+1,j),1,b(j+1),1)
+      b(j) = b(j)/t(j,j)
+    end do
     100       continue
-           go to 140
+    go to 140
     110    continue
-              b(1) = b(1)/t(1,1)
-              if (n .lt. 2) go to 130
-              do 120 j = 2, n
-                 b(j) = b(j) - ddot(j-1,t(1,j),1,b(1),1)
-                 b(j) = b(j)/t(j,j)
-    120       continue
+    b(1) = b(1)/t(1,1)
+    if (n .lt. 2) go to 130
+    do j = 2, n
+      b(j) = b(j) - ddot(j-1,t(1,j),1,b(1),1)
+      b(j) = b(j)/t(j,j)
+    end do
     130       continue
     140    continue
     150 continue
-        return
-end subroutine dtrsl
+  end subroutine dtrsl
 
 end module module_blas_etc
