b80779ddb99c0a4b97ef5ff9f2bebedc7ad1efdb
Maximilien Levesque <maximilien.levesque@gmail.com>
2016-05-31 14:50:56 +0200

    Cmake choses dynamically if it compiles with MPI or not

--- src/module_mpi.F90
+++ src/module_mpi.F90
@@ -1,28 +1,31 @@
 module module_mpi
 
-  use precision_kinds, only: dp
-  implicit none
-
   !
   ! MPI header - no idea if it is a good one, the most modern one, etc. That's for testing.
   !
+#ifndef MPI_FORTRAN_HAS_MODULE
   include 'mpif.h'
+#else
+  use mpi
+#endif
+  
+  implicit none
 
-  type :: mpi_type
+  type :: impi_type
     integer :: nproc, rang, code
-  end type
-  type(mpi_type) :: mpi
+  end type impi_type
+  type(impi_type) :: impi
 
 contains
 
   subroutine init
     implicit none
-    call mpi_init(mpi%code)
+    call mpi_init(impi%code)
   end subroutine init
 
   subroutine finalize
     implicit none
-    call mpi_finalize(mpi%code)
+    call mpi_finalize(impi%code)
   end subroutine finalize
 
 end module module_mpi
