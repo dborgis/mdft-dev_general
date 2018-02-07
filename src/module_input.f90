dborgis/mdft-dev_general

commit 42140c2ac601b2ac59d96b12e49156af513876df

Input file name defined by user only once, not twice anymore
In order to change the input file name, dft.in in MDFT, to anything like hugo.in, the user had to search for dft.in
in the whole module, and to change it at two locations. The second location is now replaced by a smart :P use of
the first declaration.

src/module_input.f90
@@ -8,7 +8,7 @@ module module_input
   ! since this module should be shared between several codes,
   ! at least laboetie and mdft,
   ! it should have as few dependancies as possible.
-  use precision_kinds, only: dp
+  use kinds, only: dp
 
   implicit none
 
@@ -652,7 +652,7 @@ end function input_char
 
     ! print what has been considered as input by the parser, that is what is contained by input_line(), to output dir.
     call execute_command_line("mkdir -p output", WAIT=.TRUE.)  ! just create folder. If it already exists, nothing happens.
-    call execute_command_line ("cp dft.in output/.")
+    call execute_command_line( "cp "//inputfilename//" output/." )
     open(10, FILE='output/inputfile.out' )
     block
       integer :: i

commit ce5bc3b2e210ad96cc7fbe1438992aecb727dabe

Fix typo in use module precision_kinds
spotted by @guillaumejeanmairet  
introduced in commit 42140c2ac601b2ac59d96b12e49156af513876df

src/module_input.f90
@@ -8,7 +8,7 @@ module module_input
   ! since this module should be shared between several codes,
   ! at least laboetie and mdft,
   ! it should have as few dependancies as possible.
-  use kinds, only: dp
+  use precision_kinds, only: dp
 
   implicit none
 